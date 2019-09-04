93cd3e2864329771bfdaaa88fea797b0f06f572b
Petter Oeberg <petter.oeberg@vmc-motion.com>
2019-09-04 09:07:06 +0200

    Create Editor module for AGXUnreal plugin

---
+++ Source/AGXUnrealEditor/Private/AGXUnrealEditor.cpp
@@ -0,0 +1,15 @@
+#include "AGXUnrealEditor.h"
+
+#define LOCTEXT_NAMESPACE "FAGXUnrealEditorModule"
+
+void FAGXUnrealEditorModule::StartupModule()
+{
+}
+
+void FAGXUnrealEditorModule::ShutdownModule()
+{
+}
+
+#undef LOCTEXT_NAMESPACE
+
+IMPLEMENT_MODULE(FAGXUnrealEditorModule, AGXUnrealEditor);
