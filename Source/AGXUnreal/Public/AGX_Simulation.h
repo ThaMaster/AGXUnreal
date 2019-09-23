d9f34dd9b690a20ec0770fdee08971e5da46378a
Martin Nilsson <martin.nilsson@algoryx.se>
2019-09-23 16:01:50 +0200

    Automatic code cleanup in AGX_Simulation.h

--- Source/AGXUnreal/Public/AGX_Simulation.h
+++ Source/AGXUnreal/Public/AGX_Simulation.h
@@ -9,17 +9,18 @@
 /**
  * Manages an AGX simulation instance.
  *
- * When an instance of this class exists, native AGX objects relating to the simulation
- * can be created (RigidBody, Constraint, ContactMaterial, etc). When this instance
- * is destroyed, all those native objects must be destroyed.
+ * When an instance of this class exists, native AGX objects relating to the
+ * simulation can be created (RigidBody, Constraint, ContactMaterial, etc).
+ * When this instance is destroyed, all those native objects must be destroyed.
  *
- * Lifetime is bound to a GameInstance. Therefore, each playing GameInstance will have
- * exactly one UAGX_Simulation instance.
+ * Lifetime is bound to a GameInstance. Therefore, each playing GameInstance
+ * will have exactly one UAGX_Simulation instance.
  *
- * When not playing, the CDO (class default object) can be modified through the Editor UI.
- * This is useful for setting simulation properties like time step and contact materials.
- * When a GameInstance is started, the properties of the CDO will automatically be copied
- * over by Unreal Engine to the GameInstance specific UAGX_Simulation instance.
+ * When not playing, the CDO (class default object) can be modified through the
+ * Editor UI. This is useful for setting simulation properties like time step
+ * and contact materials. When a GameInstance is started, the properties of the
+ * CDO will automatically be copied over by Unreal Engine to the GameInstance
+ * specific UAGX_Simulation instance.
  *
  */
 UCLASS(config = Engine, defaultconfig)
@@ -28,20 +29,17 @@ class AGXUNREAL_API UAGX_Simulation : public UGameInstanceSubsystem
 	GENERATED_BODY()
 
 public:
-
 	/** Step length of the integrator, in seconds. 0.0167 by default. */
-	UPROPERTY(config, EditAnywhere, Category = "Solver", meta = (ClampMin = "0.001", UIMin = "0.001", ClampMax = "1.0", UIMax = "1.0"))
-	float TimeStep = 1.0 / 60.0;
+	UPROPERTY(config, EditAnywhere, Category = "Solver",
+		meta = (ClampMin = "0.001", UIMin = "0.001", ClampMax = "1.0", UIMax = "1.0"))
+	float TimeStep = 1.0f / 60.0f;
 
 	/** Uniform default scene gravity, in cm/s^2. -980.665 by default. */
 	UPROPERTY(config, EditAnywhere, Category = "Scene Defaults")
 	FVector Gravity = FVector(0.0f, 0.0f, -980.665f);
 
-
-public: // OVERRIDES
-
+public:	// OVERRIDES
 	void Initialize(FSubsystemCollectionBase& Collection) override;
 
 	void Deinitialize() override;
-
 };
