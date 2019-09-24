be849fb9c0becc069eb1b36095c70ce814e7f30e
Martin Nilsson <martin.nilsson@algoryx.se>
2019-09-24 14:36:14 +0200

    Add conversion between agx::RigidBody::MotionControl and EAGX_MotionControl

--- Source/AGXUnrealBarrier/Private/TypeConversions.h
+++ Source/AGXUnrealBarrier/Private/TypeConversions.h
@@ -1,8 +1,14 @@
 #pragma once
 
+/// \todo This may become a header file with lots of includes, which will make
+///       it a compile time hog. Consider splitting it  up.
+
+#include "AGX_MotionControl.h"
+
 #include <Math/Vector.h>
 
 #include <agx/Vec3.h>
+#include <agx/RigidBody.h>
 
 inline float Convert(agx::Real V)
 {
@@ -22,4 +28,34 @@ inline FVector Convert(agx::Vec3 V)
 inline agx::Vec3 Convert(FVector V)
 {
 	return agx::Vec3(Convert(V.X), Convert(V.Y), Convert(V.Z));
+}
+
+inline agx::RigidBody::MotionControl Convert(EAGX_MotionControl V)
+{
+	switch (V)
+	{
+		case MC_STATIC:
+			return agx::RigidBody::STATIC;
+		case MC_KINEMATICS:
+			return agx::RigidBody::KINEMATICS;
+		case MC_DYNAMICS:
+			return agx::RigidBody::DYNAMICS;
+	}
+	/// \todo Add UE_LOG(LogAGX, ...) here.
+	return agx::RigidBody::DYNAMICS;
+}
+
+inline EAGX_MotionControl Convert(agx::RigidBody::MotionControl V)
+{
+	switch (V)
+	{
+		case agx::RigidBody::STATIC:
+			return MC_STATIC;
+		case agx::RigidBody::KINEMATICS:
+			return MC_KINEMATICS;
+		case agx::RigidBody::DYNAMICS:
+			return MC_DYNAMICS;
+	}
+	/// \todo Add UE_LOG(LogAGX, ...) here.
+	return MC_KINEMATICS;
 }
