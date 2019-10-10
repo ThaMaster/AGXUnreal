42e4ece47e606bb8c8a9ba6c182040f4b0397b9b
Petter Oeberg <petter.oeberg@vmc-motion.com>
2019-10-10 13:25:00 +0900

    Move constraint structs and enums to separate files

---
+++ Source/AGXUnreal/Public/Constraints/AGX_ConstraintEnums.h
@@ -0,0 +1,50 @@
+#pragma once
+
+#include "CoreMinimal.h"
+
+
+/**
+ * Constraint type independent index of Degree of Freedom(DOF).Does never change
+ * index order layout, even in derived constraints, contrary to the AGX's native
+ * constraint - specific DOF indexes.
+ */
+UENUM()
+enum class EGenericDofIndex
+{
+	/** All degrees of freedom */
+	ALL_DOF = -1			UMETA(DisplayName = "All"),
+
+	/** DOF for the first translational axis */
+	TRANSLATIONAL_1 = 0		UMETA(DisplayName = "Translation1"),
+
+	/** DOF for the second translational axis */
+	TRANSLATIONAL_2 = 1		UMETA(DisplayName = "Translation2"),
+
+	/** DOF for the third translational axis */
+	TRANSLATIONAL_3 = 2		UMETA(DisplayName = "Translation3"),
+
+	/** DOF corresponding to the first rotational axis */
+	ROTATIONAL_1 = 3		UMETA(DisplayName = "Rotation1"),
+
+	/** DOF corresponding to the second rotational axis */
+	ROTATIONAL_2 = 4		UMETA(DisplayName = "Rotation2"),
+
+	/** DOF for rotation around Z-axis */
+	ROTATIONAL_3 = 5		UMETA(DisplayName = "Rotation3"),
+};
+
+
+/**
+ * Flags used to be able to identify DOFs and combine them into a bitmask.
+ */
+UENUM(meta = (Bitflags))
+enum class EDofFlag : uint8
+{
+	DOF_FLAG_ALL = 0x3F						UMETA(DisplayName = "All"),
+	DOF_FLAG_TRANSLATIONAL_1 = 1 << 0		UMETA(DisplayName = "Translation1"),
+	DOF_FLAG_TRANSLATIONAL_2 = 1 << 1		UMETA(DisplayName = "Translation2"),
+	DOF_FLAG_TRANSLATIONAL_3 = 1 << 2		UMETA(DisplayName = "Translation3"),
+	DOF_FLAG_ROTATIONAL_1 = 1 << 3			UMETA(DisplayName = "Rotation1"),
+	DOF_FLAG_ROTATIONAL_2 = 1 << 4			UMETA(DisplayName = "Rotation2"),
+	DOF_FLAG_ROTATIONAL_3 = 1 << 5			UMETA(DisplayName = "Rotation3"),
+};
