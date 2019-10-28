8b4a16ad427b75128efdefcb1e0f63b05a228f23
Petter Oeberg <petter.oeberg@vmc-motion.com>
2019-10-28 11:34:44 +0900

    Fix so that Constraint Frames visualization does not block interaction with the transform gizmo
    The way this is fixed is simply by making sure that the lines used to visualize constraint frames are always thinner than those of the transform gizmo, so that the transform gizmo is thickest in the scenario where they are overlapping. This scenario should be solvable by other means (HitProxy, depth sorting, etc), but it did not seem to work in the particular scenario when the transform gizmo and coordinate frame visualization are within a mesh while the viewport camera is outside the mesh (i.e. occlusion by the mesh).

--- Source/AGXUnrealEditor/Private/Constraints/AGX_ConstraintComponentVisualizer.cpp
+++ Source/AGXUnrealEditor/Private/Constraints/AGX_ConstraintComponentVisualizer.cpp
@@ -17,7 +17,20 @@ namespace
 	const FColor RigidBodyHighlightColor(243, 139, 0);
 	const float RigidBodyHighlightThickness(3.0f);
 	const float FrameGizmoScale(70.0f);
-	const float FrameGizmoThickness(5.0f);
+	const float FrameGizmoThickness(3.0f);
+
+
+	void DrawCoordinateSystemAxes(FPrimitiveDrawInterface* PDI, FVector const& AxisLoc, FRotator const& AxisRot, float Scale, uint8 DepthPriority, float Thickness, float DepthBias, bool bScreenSpace)
+	{
+		FRotationMatrix R(AxisRot);
+		FVector const X = R.GetScaledAxis(EAxis::X);
+		FVector const Y = R.GetScaledAxis(EAxis::Y);
+		FVector const Z = R.GetScaledAxis(EAxis::Z);
+
+		PDI->DrawLine(AxisLoc, AxisLoc + X * Scale, FLinearColor::Red, DepthPriority, Thickness, DepthBias, bScreenSpace);
+		PDI->DrawLine(AxisLoc, AxisLoc + Y * Scale, FLinearColor::Green, DepthPriority, Thickness, DepthBias, bScreenSpace);
+		PDI->DrawLine(AxisLoc, AxisLoc + Z * Scale, FLinearColor::Blue, DepthPriority, Thickness, DepthBias, bScreenSpace);
+	}
 }
 
 
@@ -57,15 +70,24 @@ void FAGX_ConstraintComponentVisualizer::DrawConstraint(const AAGX_Constraint* C
 				DrawWireBox(PDI,
 					AABB, RigidBodyHighlightColor, SDPG_World,
 					RigidBodyHighlightThickness, /*DepthBias*/ 0.0f, /*bScreenSpace*/ true);
-
 			}
 
 			// Draw gizmo for final Attachment Frame
 			{
-				DrawCoordinateSystem(PDI,
+				// It is important to make sure that drawn coordinate system does not interfere
+				// with the default transform gizmo. Therefore, make sure thickness of drawn coordinate
+				// system is thinner than transform gizmo, so that the transform gizmo is always selectable.
+				// 
+				// If is not thinner, in the scenario where the coordinate system equals the currently active
+				// transform gizmo (i.e. visually overlapping), and they are located inside a mesh while the
+				// camera is ouside of the mesh, there are difficulties	selecting the transform gizmo (even if
+				// HitProxy, depth bias, etc are used). 
+				
+				DrawCoordinateSystemAxes(PDI,
 					BodyAttachment.GetGlobalFrameLocation(),
 					BodyAttachment.GetGlobalFrameRotation().Rotator(),
-					FrameGizmoScale, SDPG_Foreground, FrameGizmoThickness);
+					FrameGizmoScale, SDPG_Foreground,
+					FrameGizmoThickness, /*DepthBias*/ 0.0f, /*bScreenSpace*/ true);
 			}
 		}
 	}
