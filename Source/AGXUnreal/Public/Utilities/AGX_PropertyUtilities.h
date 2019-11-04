9120a31c811d95f82ca58525d7407b767ba349b5
Martin Nilsson <martin.nilsson@algoryx.se>
2019-11-04 09:19:41 +0100

    Do not add `static` to an out-of-class definition of a static member function
    Also Unreal code style fixes.

--- Source/AGXUnreal/Public/Utilities/AGX_PropertyUtilities.h
+++ Source/AGXUnreal/Public/Utilities/AGX_PropertyUtilities.h
@@ -7,31 +7,30 @@
 #include "PropertyHandle.h"
 #include "UnrealType.h"
 
-
 /**
  * Provides helper functions for working with UProperty and IPropertyHandle.
  */
 class AGXUNREAL_API FAGX_PropertyUtilities
 {
 public:
+	static bool PropertyEquals(const TSharedPtr<IPropertyHandle>& First, const TSharedPtr<IPropertyHandle>& Second);
 
-	static bool	PropertyEquals(const TSharedPtr<IPropertyHandle> &First, const TSharedPtr<IPropertyHandle> &Second);
+	static UObject* GetParentObjectOfStruct(const TSharedPtr<IPropertyHandle>& StructPropertyHandle);
 
-	static UObject*	GetParentObjectOfStruct(const TSharedPtr<IPropertyHandle> &StructPropertyHandle);
+	static UObject* GetObjectFromHandle(const TSharedPtr<IPropertyHandle>& PropertyHandle);
 
-	static UObject* GetObjectFromHandle(const TSharedPtr<IPropertyHandle> &PropertyHandle);
-
-	template<typename TStruct, typename TOwningClass>
-	static TStruct* GetStructFromHandle(const TSharedPtr<IPropertyHandle> &PropertyHandle, TOwningClass *OwningClass);
+	template <typename TStruct, typename TOwningClass>
+	static TStruct* GetStructFromHandle(const TSharedPtr<IPropertyHandle>& PropertyHandle, TOwningClass* OwningClass);
 };
 
-
-template<typename TStruct, typename TOwningClass>
-static TStruct* FAGX_PropertyUtilities::GetStructFromHandle(const TSharedPtr<IPropertyHandle> &PropertyHandle, TOwningClass *OwningClass)
+template <typename TStruct, typename TOwningClass>
+TStruct* FAGX_PropertyUtilities::GetStructFromHandle(
+	const TSharedPtr<IPropertyHandle>& PropertyHandle, TOwningClass* OwningClass)
 {
-	if (PropertyHandle->IsValidHandle())
-		return PropertyHandle->GetProperty()->ContainerPtrToValuePtr<TStruct>(OwningClass);
-	else
+	if (!PropertyHandle->IsValidHandle())
+	{
 		return nullptr;
-}
+	}
 
+	return PropertyHandle->GetProperty()->ContainerPtrToValuePtr<TStruct>(OwningClass);
+}
