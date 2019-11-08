594836a8a95a98b1a6c4060095ed4567706f090a
Martin Nilsson <martin.nilsson@algoryx.se>
2019-11-08 16:05:46 +0100

    Add getter for the total local transformation for shape barrier
    Includes both the geometry-relative-body transformation and the
    shape-relative-geometry transformation to produce a shape-relative-body
    transformation.

--- Source/AGXUnrealBarrier/Private/Shapes/ShapeBarrier.cpp
+++ Source/AGXUnrealBarrier/Private/Shapes/ShapeBarrier.cpp
@@ -6,12 +6,12 @@
 #include "Misc/AssertionMacros.h"
 
 FShapeBarrier::FShapeBarrier()
-	: NativeRef{new FGeometryAndShapeRef}
+	: NativeRef {new FGeometryAndShapeRef}
 {
 }
 
 FShapeBarrier::FShapeBarrier(std::unique_ptr<FGeometryAndShapeRef> Native)
-	: NativeRef{std::move(Native)}
+	: NativeRef {std::move(Native)}
 {
 }
 
@@ -59,14 +59,61 @@ const FGeometryAndShapeRef* FShapeBarrier::GetNative() const
 	return NativeRef.get();
 }
 
-void FShapeBarrier::SetLocalPosition(const FVector &Position, UWorld* World)
+void FShapeBarrier::SetLocalPosition(const FVector& Position, UWorld* World)
 {
 	check(HasNative());
 	NativeRef->NativeGeometry->setLocalPosition(ConvertVector(Position, World));
 }
 
-void FShapeBarrier::SetLocalRotation(const FQuat &Rotation)
+void FShapeBarrier::SetLocalRotation(const FQuat& Rotation)
 {
 	check(HasNative());
 	NativeRef->NativeGeometry->setLocalRotation(Convert(Rotation));
+}
+
+FVector FShapeBarrier::GetLocalPosition(UWorld* World) const
+{
+	return std::get<0>(GetLocalPositionAndRotation(World));
+}
+
+FQuat FShapeBarrier::GetLocalRotation(UWorld* World) const
+{
+	return std::get<1>(GetLocalPositionAndRotation(World));
+}
+
+namespace
+{
+	agxCollide::ShapeIterator FindShape(agxCollide::Geometry* Geometry, agxCollide::Shape* Shape)
+	{
+		agxCollide::ShapeIterator Iterator(Geometry);
+		while (Iterator.isValid() && Iterator.getShape() != Shape)
+		{
+			Iterator.next();
+		}
+		if (!Iterator.isValid())
+		{
+			UE_LOG(LogTemp, Error,
+				TEXT("Found invalid FShapeBarrier. The native Geometry does not contain the native Shape."));
+		}
+		return Iterator;
+	}
+}
+
+std::tuple<FVector, FQuat> FShapeBarrier::GetLocalPositionAndRotation(UWorld* World) const
+{
+	check(HasNative());
+	agxCollide::ShapeIterator Iterator = FindShape(NativeRef->NativeGeometry, NativeRef->NativeShape);
+	if (!Iterator.isValid())
+	{
+		return {FVector::ZeroVector, FQuat::Identity};
+	}
+
+	// The ShapeTransform is always Identity when the Native objects have been
+	// created from AGXUnreal objects. However, it can be a non-Identity transform
+	// during import from a .agx archive. Split this implementation if this step
+	// is shown to be a performance problem.
+	const agx::AffineMatrix4x4& GeometryTransform = NativeRef->NativeGeometry->getLocalTransform();
+	const agx::AffineMatrix4x4& ShapeTransform = Iterator.getLocalTransform();
+	const agx::AffineMatrix4x4 ShapeRelativeBody = ShapeTransform * GeometryTransform;
+	return {ConvertVector(ShapeRelativeBody.getTranslate(), World), Convert(ShapeRelativeBody.getRotate())};
 }
