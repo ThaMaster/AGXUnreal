f3e6f4a6de7c53e4608027e3b3e4dbe4ccdcd4b8
Martin Nilsson <martin.nilsson@algoryx.se>
2019-11-25 11:28:20 +0100

    Pass Unreal landscape heights on to AGX Dynamics heightfield

--- Source/AGXUnrealBarrier/Private/Shapes/HeightFieldShapeBarrier.cpp
+++ Source/AGXUnrealBarrier/Private/Shapes/HeightFieldShapeBarrier.cpp
@@ -4,6 +4,19 @@
 #include "ShapeBarrierImpl.h"
 #include "TypeConversions.h"
 
+namespace
+{
+	agxCollide::HeightField* NativeHeightField(FHeightFieldShapeBarrier* Barrier)
+	{
+		return Barrier->GetNative()->NativeShape->as<agxCollide::HeightField>();
+	}
+
+	const agxCollide::HeightField* NativeHeightField(const FHeightFieldShapeBarrier* Barrier)
+	{
+		return Barrier->GetNative()->NativeShape->as<agxCollide::HeightField>();
+	}
+}
+
 FHeightFieldShapeBarrier::FHeightFieldShapeBarrier()
 	: FShapeBarrier()
 {
@@ -33,11 +46,19 @@ void FHeightFieldShapeBarrier::AllocateNativeHeightField(
 	int32 NumVerticesX, int32 NumVerticesY, float SizeX, float SizeY, const TArray<float>& Heights)
 {
 	check(!HasNative());
+	check(Heights.Num() >= 0);
+
 	agx::Real SizeXAGX = ConvertDistance(SizeX);
 	agx::Real SizeYAGX = ConvertDistance(SizeY);
 
 	NativeRef->NativeShape = new agxCollide::HeightField(
 		static_cast<size_t>(NumVerticesX), static_cast<size_t>(NumVerticesY), SizeXAGX, SizeYAGX);
+
+	agx::VectorPOD<agx::Real> HeightsAGX;
+	HeightsAGX.reserve(static_cast<size_t>(Heights.Num()));
+	auto ConvertHeight = static_cast<agx::Real(*)(float)>(ConvertDistance);
+	std::transform(Heights.begin(), Heights.end(), std::back_inserter(HeightsAGX), ConvertHeight);
+	NativeHeightField(this)->setHeights(HeightsAGX);
 }
 
 void FHeightFieldShapeBarrier::AllocateNativeShape()
