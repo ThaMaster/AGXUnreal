e9d41360dc83529f31e8c6be24ac9365a6c68ca0
Josef Holmner <josef.holmner@algoryx.se>
2019-12-09 11:51:20 +0100

    Export LogAGX from the AGXUnreal module

--- Source/AGXUnreal/Public/AGX_LogCategory.h
+++ Source/AGXUnreal/Public/AGX_LogCategory.h
@@ -10,7 +10,7 @@
  *
  * Default verbosity is set to 'Log', and will be used when one is not specified in the
  * ini files or on the command line. Anything more verbose than this will not be logged.
- * 
+ *
  * Compile time verbosity is set to 'All'.
  */
-DECLARE_LOG_CATEGORY_EXTERN(LogAGX, Log, All);
+AGXUNREAL_API DECLARE_LOG_CATEGORY_EXTERN(LogAGX, Log, All);
