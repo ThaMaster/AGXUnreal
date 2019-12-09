ede18965a145dbd72e3456ce0b26e158c89c221f
Martin Nilsson <martin.nilsson@algoryx.se>
2019-12-09 13:35:00 +0100

    CreateAGX_VectorComponent

---
+++ Source/AGXUnrealEditor/Public/AGX_VectorComponent.h
@@ -0,0 +1,20 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Components/PrimitiveComponent.h"
+#include "AGX_VectorComponent.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class AGXUNREALEDITOR_API UAGX_VectorComponent : public UPrimitiveComponent
+{
+	GENERATED_BODY()
+	
+	
+	
+	
+};
