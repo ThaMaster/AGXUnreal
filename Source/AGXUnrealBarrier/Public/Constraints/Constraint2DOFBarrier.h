f32bf3d76c970d6098a52e3b112ed35aecdc315d
Martin Nilsson <martin.nilsson@algoryx.se>
2020-01-28 12:03:12 +0100

    Add controller barrier getters to FConstraint2DOFBarrier
    So that we can get controller data out of constraints imported from .agx
    archives.

--- Source/AGXUnrealBarrier/Public/Constraints/Constraint2DOFBarrier.h
+++ Source/AGXUnrealBarrier/Public/Constraints/Constraint2DOFBarrier.h
@@ -27,19 +27,36 @@ public:
 	void SetElectricMotorController(
 		const FElectricMotorControllerBarrier& Controller, int32 SecondaryConstraintIndex);
 
+	void GetElectricMotorController(
+		FElectricMotorControllerBarrier& Controller, int32 SecondaryConstraintIndex) const;
+
 	void SetFrictionController(
 		const FFrictionControllerBarrier& Controller, int32 SecondaryConstraintIndex);
+
+	void GetFrictionController(
+		FFrictionControllerBarrier& Controller, int32 SecondaryConstraintIndex) const;
 
 	void SetLockController(
 		const FLockControllerBarrier& Controller, int32 SecondaryConstraintIndex);
 
+	void GetLockController(
+		FLockControllerBarrier& Controller, int32 SecondaryConstraintIndex) const;
+
 	void SetRangeController(
 		const FRangeControllerBarrier& Controller, int32 SecondaryConstraintIndex);
+
+	void GetRangeController(
+		FRangeControllerBarrier& Controller, int32 SecondaryConstraintIndex) const;
 
 	void SetTargetSpeedController(
 		const FTargetSpeedControllerBarrier& Controller, int32 SecondaryConstraintIndex);
 
+	void GetTargetSpeedController(
+		FTargetSpeedControllerBarrier& Controller, int32 SecondaryConstraintIndex) const;
+
 	void SetScrewController(const FScrewControllerBarrier& Controller);
+
+	void GetScrewController(FScrewControllerBarrier& Controller) const;
 
 private:
 	FConstraint2DOFBarrier(const FConstraint2DOFBarrier&) = delete;
