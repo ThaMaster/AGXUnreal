299314d577dc53830e2bc47d8fe130159bc6a183
Martin Nilsson <martin.nilsson@algoryx.se>
2020-01-29 14:30:19 +0100

    Add tooltips to InputTargetSpeed properties

--- Source/AGXUnreal/Public/Constraints/AGX_InputTargetSpeed.h
+++ Source/AGXUnreal/Public/Constraints/AGX_InputTargetSpeed.h
@@ -7,7 +7,6 @@
 #include "InputCoreTypes.h"
 #include "AGX_InputTargetSpeed.generated.h"
 
-
 /**
  * Component that checks for particular keyboard presses and set a constraint
  * motor speed in response.
@@ -22,19 +21,30 @@ class AGXUNREAL_API UAGX_InputTargetSpeed : public UActorComponent
 public:
 	UAGX_InputTargetSpeed();
 
-	UPROPERTY(EditAnywhere, Category = "AGX Constraint Speed Input")
+	UPROPERTY(
+		EditAnywhere, Category = "AGX Constraint Speed Input",
+		meta = (Tooltip = "Key to trigger forward motion of the constraint."))
 	FKey ForwardKey;
 
-	UPROPERTY(EditAnywhere, Category = "AGX Constraint Speed Input")
+	UPROPERTY(
+		EditAnywhere, Category = "AGX Constraint Speed Input",
+		meta = (Tooltip = "The constraint speed forward. cm/s or degrees/s."))
 	float ForwardSpeed;
 
-	UPROPERTY(EditAnywhere, Category = "AGX Constraint Speed Input")
+	UPROPERTY(
+		EditAnywhere, Category = "AGX Constraint Speed Input",
+		meta = (Tooltip = "Key to trigger backward motion of the constraint."))
 	FKey BackwardKey;
 
-	UPROPERTY(EditAnywhere, Category = "AGX Constraint Speed Input")
+	UPROPERTY(
+		EditAnywhere, Category = "AGX Constraint Speed Input",
+		meta = (Tooltip = "The constraint speed backward. cm/s or degrees/s."))
 	float BackwardSpeed;
 
-	UPROPERTY(EditAnywhere, Category = "AGX Constraint Speed Input")
+	UPROPERTY(
+		EditAnywhere, Category = "AGX Constraint Speed Input",
+		meta =
+			(Tooltip = "If checked the constraint will move freely when no key is held. If unchecked the constraint will be stopped when no key is held."))
 	bool bDisableOnRelease;
 
 protected:
