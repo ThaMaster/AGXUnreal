c13ec8636a9dffbb38761b072971e05dbf215285
Martin Nilsson <martin.nilsson@algoryx.se>
2020-01-31 09:52:28 +0100

    Fix comment typo in ConstraintBarrier.h

--- Source/AGXUnrealBarrier/Public/Constraints/ConstraintBarrier.h
+++ Source/AGXUnrealBarrier/Public/Constraints/ConstraintBarrier.h
@@ -81,7 +81,7 @@ private:
 
 private:
 	/**
-	 * Called from AllocateNative. Each subclass msut override this function and within
+	 * Called from AllocateNative. Each subclass must override this function and within
 	 * it create the correct agx::Constraint object, and assign it to NativeRef->Native.
 	 *
 	 * The override should not call the override of the parent class, to avoid multiple
