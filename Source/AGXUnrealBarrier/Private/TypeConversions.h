3a167a03189204fcc94bda7cd1d725c638ef2927
Martin Nilsson <martin.nilsson@algoryx.se>
2020-01-31 13:49:23 +0100

    Add comments and reorder conversion functions in TypeConversion
    Trying to group similar functions next to each other, and moving simpler
    ones above more complicated/specialized ones.

--- Source/AGXUnrealBarrier/Private/TypeConversions.h
+++ Source/AGXUnrealBarrier/Private/TypeConversions.h
@@ -25,6 +25,7 @@
 #include <agx/Quat.h>
 #include <agx/Vec3.h>
 #include "EndAGXIncludes.h"
+
 /// \note These functions assume that agx::Real and float are different types.
 /// They also assume that agx::Real has higher (or equal) precision than float.
 
@@ -37,20 +38,23 @@ namespace
 	constexpr T UNREAL_TO_AGX_DISTANCE_FACTOR = T(0.01);
 }
 
+
+// Scalars.
+
 inline float Convert(agx::Real V)
 {
 	return static_cast<float>(V);
 }
 
-inline float ConvertDistance(agx::Real V)
+inline float ConvertDistance(agx::Real D)
 {
-	return static_cast<float>(V * AGX_TO_UNREAL_DISTANCE_FACTOR<agx::Real>);
+	return static_cast<float>(D * AGX_TO_UNREAL_DISTANCE_FACTOR<agx::Real>);
 }
 
 template <typename T>
-inline T ConvertDistanceToUnreal(agx::Real V)
+inline T ConvertDistanceToUnreal(agx::Real D)
 {
-	return static_cast<T>(V * AGX_TO_UNREAL_DISTANCE_FACTOR<agx::Real>);
+	return static_cast<T>(D * AGX_TO_UNREAL_DISTANCE_FACTOR<agx::Real>);
 }
 
 inline float ConvertAngle(agx::Real A)
@@ -70,9 +74,9 @@ inline agx::Real ConvertDistance(float V)
 }
 
 template <typename T>
-inline agx::Real ConvertDistanceToAgx(T V)
+inline agx::Real ConvertDistanceToAgx(T D)
 {
-	return static_cast<agx::Real>(V) * UNREAL_TO_AGX_DISTANCE_FACTOR<agx::Real>;
+	return static_cast<agx::Real>(D) * UNREAL_TO_AGX_DISTANCE_FACTOR<agx::Real>;
 }
 
 inline agx::Real ConvertAngle(float A)
@@ -80,6 +84,8 @@ inline agx::Real ConvertAngle(float A)
 	return FMath::DegreesToRadians(Convert(A));
 }
 
+
+// Vectors.
 
 inline FVector Convert(agx::Vec3 V)
 {
@@ -113,6 +119,9 @@ inline agx::Vec3 ConvertVector(const FVector& V)
 	return agx::Vec3(ConvertDistance(V.X), -ConvertDistance(V.Y), ConvertDistance(V.Z));
 }
 
+
+// Interval/Range.
+
 inline FFloatInterval Convert(const agx::RangeReal& R)
 {
 	return FFloatInterval(Convert(R.lower()), Convert(R.upper()));
@@ -133,6 +142,11 @@ inline agx::RangeReal ConvertDistance(const FFloatInterval& I)
 	return agx::RangeReal(ConvertDistance(I.Min), ConvertDistance(I.Max));
 }
 
+
+// TWoVectors/Line.
+// TwoVectors may represent other things as well. If that's the case then we'll
+// need to do something else.
+
 inline agx::Line Convert(const FTwoVectors& Vs)
 {
 	return {Convert(Vs.v1), Convert(Vs.v2)};
@@ -148,6 +162,9 @@ inline agx::Line ConvertVector(const FTwoVectors& Vs)
 	return {ConvertVector(Vs.v1), ConvertVector(Vs.v2)};
 }
 
+
+// Quaternions.
+
 inline FQuat Convert(const agx::Quat& V)
 {
 	return FQuat(Convert(V.x()), -Convert(V.y()), Convert(V.z()), -Convert(V.w()));
@@ -158,35 +175,8 @@ inline agx::Quat Convert(const FQuat& V)
 	return agx::Quat(Convert(V.X), -Convert(V.Y), Convert(V.Z), -Convert(V.W));
 }
 
-inline agx::RigidBody::MotionControl Convert(EAGX_MotionControl V)
-{
-	switch (V)
-	{
-		case MC_STATIC:
-			return agx::RigidBody::STATIC;
-		case MC_KINEMATICS:
-			return agx::RigidBody::KINEMATICS;
-		case MC_DYNAMICS:
-			return agx::RigidBody::DYNAMICS;
-	}
-	/// \todo Add UE_LOG(LogAGX, ...) here.
-	return agx::RigidBody::DYNAMICS;
-}
 
-inline EAGX_MotionControl Convert(agx::RigidBody::MotionControl V)
-{
-	switch (V)
-	{
-		case agx::RigidBody::STATIC:
-			return MC_STATIC;
-		case agx::RigidBody::KINEMATICS:
-			return MC_KINEMATICS;
-		case agx::RigidBody::DYNAMICS:
-			return MC_DYNAMICS;
-	}
-	/// \todo Add UE_LOG(LogAGX, ...) here.
-	return MC_KINEMATICS;
-}
+// Text.
 
 inline FString Convert(const agx::String& StringAGX)
 {
@@ -227,22 +217,92 @@ inline agx::Uuid Convert(const FGuid& Guid)
 	return Uuid;
 }
 
-/**
- * Given a Barrier, returns the final AGX native object.
- */
-template <typename TNative, typename TBarrier>
-TNative* GetNativeFromBarrier(const TBarrier* Barrier)
+
+// Enumerations.
+
+inline agx::RigidBody::MotionControl Convert(EAGX_MotionControl V)
 {
-	if (Barrier && Barrier->HasNative())
-		return Barrier->GetNative()->Native.get();
-	else
-		return nullptr;
+	switch (V)
+	{
+		case MC_STATIC:
+			return agx::RigidBody::STATIC;
+		case MC_KINEMATICS:
+			return agx::RigidBody::KINEMATICS;
+		case MC_DYNAMICS:
+			return agx::RigidBody::DYNAMICS;
+	}
+	/// \todo Add UE_LOG(LogAGX, ...) here.
+	return agx::RigidBody::DYNAMICS;
+}
+
+inline EAGX_MotionControl Convert(agx::RigidBody::MotionControl V)
+{
+	switch (V)
+	{
+		case agx::RigidBody::STATIC:
+			return MC_STATIC;
+		case agx::RigidBody::KINEMATICS:
+			return MC_KINEMATICS;
+		case agx::RigidBody::DYNAMICS:
+			return MC_DYNAMICS;
+	}
+	/// \todo Add UE_LOG(LogAGX, ...) here.
+	return MC_KINEMATICS;
+}
+
+
+inline agx::Notify::NotifyLevel ConvertLogLevelVerbosity(ELogVerbosity::Type LogVerbosity)
+{
+	switch (LogVerbosity)
+	{
+		case ELogVerbosity::VeryVerbose:
+			return agx::Notify::NOTIFY_DEBUG;
+		case ELogVerbosity::Verbose:
+			return agx::Notify::NOTIFY_DEBUG;
+		case ELogVerbosity::Log:
+			return agx::Notify::NOTIFY_INFO;
+		case ELogVerbosity::Display:
+			return agx::Notify::NOTIFY_WARNING;
+		case ELogVerbosity::Warning:
+			return agx::Notify::NOTIFY_WARNING;
+		case ELogVerbosity::Error:
+			return agx::Notify::NOTIFY_ERROR;
+		case ELogVerbosity::Fatal:
+			return agx::Notify::NOTIFY_ERROR;
+		default:
+		UE_LOG(
+			LogAGX, Warning,
+			TEXT("ConvertLogLevelVerbosity: unknown verbosity level: %d. Verbosity level "
+				 "'NOTIFY_INFO' will be used instead."),
+			LogVerbosity);
+
+			// Use NOTIFY_INFO as default, if unknown log verbosity is given
+			return agx::Notify::NOTIFY_INFO;
+	}
 }
 
 inline agx::FrameRef ConvertFrame(const FVector& FramePosition, const FQuat& FrameRotation)
 {
 	return new agx::Frame(
 		agx::AffineMatrix4x4(Convert(FrameRotation), ConvertVector(FramePosition)));
+}
+
+
+
+
+namespace
+{
+	/**
+	 * Given a Barrier, returns the final AGX native object.
+	 */
+	template<typename TNative, typename TBarrier>
+	TNative* GetNativeFromBarrier(const TBarrier* Barrier)
+	{
+		if (Barrier && Barrier->HasNative())
+			return Barrier->GetNative()->Native.get();
+		else
+			return nullptr;
+	}
 }
 
 /// \todo Consider moving this to the .cpp file.
@@ -278,36 +338,6 @@ inline void ConvertConstraintBodiesAndFrames(
 		{
 			NativeFrame2 = nullptr;
 		}
-	}
-}
-
-inline agx::Notify::NotifyLevel ConvertLogLevelVerbosity(ELogVerbosity::Type LogVerbosity)
-{
-	switch (LogVerbosity)
-	{
-		case ELogVerbosity::VeryVerbose:
-			return agx::Notify::NOTIFY_DEBUG;
-		case ELogVerbosity::Verbose:
-			return agx::Notify::NOTIFY_DEBUG;
-		case ELogVerbosity::Log:
-			return agx::Notify::NOTIFY_INFO;
-		case ELogVerbosity::Display:
-			return agx::Notify::NOTIFY_WARNING;
-		case ELogVerbosity::Warning:
-			return agx::Notify::NOTIFY_WARNING;
-		case ELogVerbosity::Error:
-			return agx::Notify::NOTIFY_ERROR;
-		case ELogVerbosity::Fatal:
-			return agx::Notify::NOTIFY_ERROR;
-		default:
-			UE_LOG(
-				LogAGX, Warning,
-				TEXT("ConvertLogLevelVerbosity: unknown verbosity level: %d. Verbosity level "
-					 "'NOTIFY_INFO' will be used instead."),
-				LogVerbosity);
-
-			// Use NOTIFY_INFO as default, if unknown log verbosity is given
-			return agx::Notify::NOTIFY_INFO;
 	}
 }
 
