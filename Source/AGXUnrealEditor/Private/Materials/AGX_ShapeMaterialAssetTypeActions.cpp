33e4f3aa7e9d78ba55d81fe02ea08364853ade1d
Martin Nilsson <martin.nilsson@algoryx.se>
2020-04-08 15:55:50 +0200

    Rename all Material-prefixed helper classes to ShapeMaterial-prefixed

---
+++ Source/AGXUnrealEditor/Private/Materials/AGX_ShapeMaterialAssetTypeActions.cpp
@@ -0,0 +1,38 @@
+#include "Materials/AGX_ShapeMaterialAssetTypeActions.h"
+
+#include "Materials/AGX_ShapeMaterialAsset.h"
+
+#define LOCTEXT_NAMESPACE "FAGX_ShapeMaterialAssetTypeActions"
+
+FAGX_ShapeMaterialAssetTypeActions::FAGX_ShapeMaterialAssetTypeActions(
+	EAssetTypeCategories::Type InAssetCategory)
+	: AssetCategory(InAssetCategory)
+{
+}
+
+FText FAGX_ShapeMaterialAssetTypeActions::GetName() const
+{
+	return LOCTEXT("AssetName", "AGX Shape Material");
+}
+
+uint32 FAGX_ShapeMaterialAssetTypeActions::GetCategories()
+{
+	return AssetCategory;
+}
+
+FColor FAGX_ShapeMaterialAssetTypeActions::GetTypeColor() const
+{
+	return FColor(255, 115, 0);
+}
+
+FText FAGX_ShapeMaterialAssetTypeActions::GetAssetDescription(const FAssetData& AssetData) const
+{
+	return LOCTEXT("AssetDescription", "Defines bulk and surface properties of AGX Shapes.");
+}
+
+UClass* FAGX_ShapeMaterialAssetTypeActions::GetSupportedClass() const
+{
+	return UAGX_ShapeMaterialAsset::StaticClass();
+}
+
+#undef LOCTEXT_NAMESPACE
