65d4e18bb63abece797d6268c05d11a6cbb4c103
Martin Nilsson <martin.nilsson@algoryx.se>
2020-05-18 12:06:56 +0200

    Add note about new BuildCMakeLib UAT tool in UE 2.25

--- Source/ThirdParty/AGXUnrealLibrary/ue-toolchain.cmake
+++ Source/ThirdParty/AGXUnrealLibrary/ue-toolchain.cmake
@@ -1,11 +1,14 @@
 # A CMake toolchain is a configuration of a set of CMake variables that make CMake generate build files in a particular
 # way. We can, for example, set the compiler to use, default compile and link flags, and which standard library to link
 # against. A toolchain is selected by passing `-DCMAKE_TOOLCHAIN_FILE=<path/to/toolchain.cmake>` the _first_ time the
-# the project is configured.
+# the CMake project is configured.
 #
 # This particular toolchain tries to configure the toolchain so that binaries compatible with Unreal Engine 4.22 is
 # produced. The flags have been taken from https://answers.unrealengine.com/questions/674473/compiling-libraries-in-linux.html
 
+# Unreal Engine 4.25 added BuildCMakeLib to the Unreal Automation Tool, UAT,
+# which may be what we actually need. Read the release notes at
+# https://docs.unrealengine.com/en-US/Support/Builds/ReleaseNotes/4_25/index.html
 
 # Unreal Engine 4.22 uses clang-7. We only name the compiler, instead of giving a full path to the binaires downloaded
 # by the Unreal Engine Setup.sh script. This makes it use the system binaries. We may want to try using the Unreal
@@ -18,7 +21,7 @@ if(NOT CMAKE_CXX_FLAGS MATCHES "Linux/LibCxx/include")
   set(CMAKE_CXX_FLAGS "${CMAKE_CXX_FLAGS} -std=c++11 -nostdinc++ -I$ENV{UE_THIRD_PARTY_DIR}/Linux/LibCxx/include/ -I$ENV{UE_THIRD_PARTY_DIR}/Linux/LibCxx/include/c++/v1" CACHE INTERNAL "")
 endif()
 
-# Disable linking with the system standard library and setup linker directoeies to the Unreal Engine paths instead.
+# Disable linking with the system standard library and setup linker directories to the Unreal Engine paths instead.
 set(UE_LINKER_FLAGS "-nodefaultlibs -L$ENV{UE_THIRD_PARTY_DIR}/Linux/LibCxx/lib/Linux/x86_64-unknown-linux-gnu/")
 if (NOT CMAKE_EXE_LINKER_FLAGS MATCHES "-nodefaultlibs")
   set(CMAKE_EXE_LINKER_FLAGS  "${CMAKE_EXE_LINKER_FLAGS} ${UE_LINKER_FLAGS}" CACHE INTERNAL "")
@@ -35,5 +38,7 @@ endif()
 set(CMAKE_C_STANDARD_LIBRARIES  "-lm -lc -lgcc_s -lgcc -lpthread" CACHE INTERNAL "")
 set(CMAKE_CXX_STANDARD_LIBRARIES  "$ENV{UE_THIRD_PARTY_DIR}/Linux/LibCxx/lib/Linux/x86_64-unknown-linux-gnu/libc++.a  $ENV{UE_THIRD_PARTY_DIR}/Linux/LibCxx/lib/Linux/x86_64-unknown-linux-gnu/libc++abi.a  -lm -lc -lgcc_s -lgcc -lpthread" CACHE INTERNAL "")
 
-# CMake fails to detect CMAKE_SIZEOF_VOID_P and it ends up being the empty string.
+# CMake fails to detect CMAKE_SIZEOF_VOID_P and it ends up being the empty
+# string. Here we assume that we are building with 8-byte pointers, i.e., 64-bit
+# mode.
 set(CMAKE_SIZEOF_VOID_P 8 CACHE INTERNAL "")
