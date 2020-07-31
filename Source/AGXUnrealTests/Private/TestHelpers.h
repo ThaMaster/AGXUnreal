e644bbb2bf91edd97be22191182a0a9f53441b55
Martin Nilsson <martin.nilsson@algoryx.se>
2020-07-31 11:26:46 +0200

    Add TestEqual for FQuat to TestHelpers since not provided by Misc/AutomationTest.h

--- Source/AGXUnrealTests/Private/TestHelpers.h
+++ Source/AGXUnrealTests/Private/TestHelpers.h
@@ -9,6 +9,20 @@ namespace TestHelpers
 		static_cast<const EAutomationTestFlags::Type>(
 			EAutomationTestFlags::ProductFilter | EAutomationTestFlags::ApplicationContextMask);
 
+	/// @todo Remove local TestEqual for FQuat once it's include in-engine.
+	/// @see "Misc/AutomationTest.h"
+	inline void TestEqual(
+		FAutomationTestBase& Test, const TCHAR* What, const FQuat& Actual, const FQuat& Expected,
+		float Tolerance = KINDA_SMALL_NUMBER)
+	{
+		if (!Expected.Equals(Actual, Tolerance))
+		{
+			Test.AddError(FString::Printf(
+				TEXT("Expected '%s' to be '%s', but it was %s within tolerance %f."), What,
+				*Expected.ToString(), *Actual.ToString(), Tolerance));
+		}
+	}
+
 }
 
 #define BAIL_TEST_IF(expression)      \
