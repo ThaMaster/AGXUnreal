f6f90433216de15a6072b80305aabdf3add36132
Martin Nilsson <martin.nilsson@algoryx.se>
2020-07-31 15:29:15 +0200

    Add world type check helper functions

--- Source/AGXUnrealTests/Private/TestHelpers.h
+++ Source/AGXUnrealTests/Private/TestHelpers.h
@@ -1,8 +1,12 @@
 
 #pragma once
 
+// Unreal Engine includes.
+#include "Engine/Engine.h"
+#include "Engine/EngineTypes.h"
 #include "Misc/AutomationTest.h"
 
+/// @todo Move most of the function definitions to TestHelper.cpp
 namespace TestHelpers
 {
 	constexpr EAutomationTestFlags::Type DefaultTestFlags =
@@ -23,6 +27,76 @@ namespace TestHelpers
 		}
 	}
 
+	/// @todo Figure out how to use UEnum::GetVAlueAsString. I get linker errors.
+	inline FString GetWorldTypeAsString(EWorldType::Type Type)
+	{
+		switch (Type)
+		{
+			case EWorldType::None:
+				return TEXT("None");
+			case EWorldType::Game:
+				return TEXT("Game");
+			case EWorldType::Editor:
+				return TEXT("Editor");
+			case EWorldType::PIE:
+				return TEXT("PIE");
+			case EWorldType::EditorPreview:
+				return TEXT("EditorPreview");
+			case EWorldType::GamePreview:
+				return TEXT("GamePreview");
+			case EWorldType::GameRPC:
+				return TEXT("GameRPC");
+			case EWorldType::Inactive:
+				return TEXT("Inactive");
+		}
+	}
+
+	// Not 'enum class' because I want implicit conversion to bool, with 'NoReason' being false.
+	// We can do 'if (Reason) { /* Cannot do world tests. */ }'.
+	enum NoWorldTestsReason
+	{
+		NoReason = 0, // It is safe to run world tests.
+		TooManyWorlds,
+		IllegalWorldType
+	};
+
+	inline FString GetNoWorldTestsReasonText(NoWorldTestsReason Reason)
+	{
+		switch (Reason)
+		{
+			case TooManyWorlds:
+				return "Cannot run tests that need a world because there are too many world "
+					   "contexts.";
+			case IllegalWorldType:
+				return FString::Printf(
+					TEXT("Cannot run tests that need a world because the available world isn't a "
+						 "'Game' world, it's a '%s' world."),
+					*TestHelpers::GetWorldTypeAsString(
+						GEngine->GetWorldContexts()[0].WorldType.GetValue()));
+		}
+		return FString();
+	}
+
+	/**
+	 * Perform the same checks as FLoadGameMapCommand in AutomationCommon, but return false instead
+	 * of crash on failure.
+	 *
+	 * @return The reason why world tests can't be run, or NoReason if world tests can be run.
+	 */
+	inline NoWorldTestsReason CanRunWorldTests()
+	{
+		const TIndirectArray<FWorldContext>& Worlds = GEngine->GetWorldContexts();
+		if (Worlds.Num() != 1)
+		{
+			return NoWorldTestsReason::TooManyWorlds;
+		}
+		if (GEngine->GetWorldContexts()[0].WorldType != EWorldType::Game)
+		{
+			return NoWorldTestsReason::IllegalWorldType;
+		}
+		return NoWorldTestsReason::NoReason;
+	}
+
 }
 
 #define BAIL_TEST_IF(expression)      \
@@ -30,4 +104,11 @@ namespace TestHelpers
 	{                                 \
 		TestFalse(#expression, true); \
 		return;                       \
+	}
+
+#define BAIL_TEST_IF_NO_WORLD()                                                   \
+	if (TestHelpers::NoWorldTestsReason Reason = TestHelpers::CanRunWorldTests()) \
+	{                                                                             \
+		AddError(TestHelpers::GetNoWorldTestsReasonText(Reason));                 \
+		return false;                                                             \
 	}
