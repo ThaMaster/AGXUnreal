90c225fada352c3d31a11760f0892702672a1dda
Josef Holmner <josef.holmner@algoryx.se>
2020-09-18 09:32:50 +0200

    fix

--- Source/AGXUnreal/Private/Terrain/AGX_Terrain.cpp
+++ Source/AGXUnreal/Private/Terrain/AGX_Terrain.cpp
@@ -534,7 +534,7 @@ bool AAGX_Terrain::InitializeParticleSystemComponent()
 		return false;
 	}
 
-#if WITH_EDITORONLY_DATA && UE_VERSION_OLDER_THAN(4, 25, 0) == false
+#if WITH_EDITORONLY_DATA && !UE_VERSION_OLDER_THAN(4, 25, 0)
 	// For a yet unknown reason, the NiagaraSystem asset must be re-compiled each time the Unreal
 	// Editor is restarted. What happens if it is not re-compiled is that no particles are rendered.
 	// The re-compilation can be done manually by opening the NiagaraSystem asset in the Unreal
