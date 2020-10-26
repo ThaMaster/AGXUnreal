9123a52649ed0f934e67b04c0ffa0112abd5e083
Martin Nilsson <martin.nilsson@algoryx.se>
2020-10-26 14:59:59 +0100

    Documentation in FMassPropertiesBarrier

--- Source/AGXUnrealBarrier/Public/MassPropertiesBarrier.h
+++ Source/AGXUnrealBarrier/Public/MassPropertiesBarrier.h
@@ -9,6 +9,13 @@
 struct FMassPropertiesPtr;
 struct FRigidBodyRef;
 
+/**
+ * MassProperties is a helper class that provide mass-related functionality to rigid bodies.
+ *
+ * A MassProperty instance is always tightly coupled with an owning RigidBody instance. Instances of
+ * FMassPropertiesBarrier should therefore not be created directly, but instead accessed through
+ * a FRigidBodyBarrier's GetMassProperties member function.
+ */
 class AGXUNREALBARRIER_API FMassPropertiesBarrier
 {
 public:
@@ -17,12 +24,31 @@ public:
 	FMassPropertiesBarrier(FMassPropertiesBarrier&& Other);
 	~FMassPropertiesBarrier();
 
+	/**
+	 * @param NewMass Mass of the rigid body, in kg.
+	 */
 	void SetMass(float NewMass);
+	/**
+	 * @return Mass of the rigid body, in kg.
+	 */
 	float GetMass() const;
 
+	/**
+	 * Set the diagonal of the body's inertia matrix.
+	 *
+	 * @param NewPrincipalInertiae
+	 */
 	void SetPrincipalInertiae(const FVector& NewPrincipalInertiae);
+
 	FVector GetPrincipalInertiae() const;
 
+	/**
+	 * Enable or disable auto-generation of mass and inertia tensor.
+	 *
+	 * Center of mass offset is always auto-generated.
+	 *
+	 * @param bAutoGenerate
+	 */
 	void SetAutoGenerate(bool bAutoGenerate);
 	bool GetAutoGenerate() const;
 
@@ -32,6 +58,17 @@ public:
 	// No AllocateNative or ReleaseNative because MassProperties are not stand-alone objects in AGX
 	// Dynamics, the are always owned by a RigidBody;
 
+	/**
+	 * Bind this MassProperties to the native MassProperties object owned by the given RigidBody.
+	 *
+	 * The given body must already have a native AGX Dynamics body.
+	 *
+	 * It is rarely necessary to call this member function anywhere outside of FRigidBodyBarrier.
+	 * Getting a pre-bound FMassPropertiesBarrier from FRigidBodyBarrier::GetMassProperties is the
+	 * recommended way.
+	 *
+	 * @param RigidBody The body for which this MassProperties should operate.
+	 */
 	void BindTo(FRigidBodyRef& RigidBody);
 
 private:
