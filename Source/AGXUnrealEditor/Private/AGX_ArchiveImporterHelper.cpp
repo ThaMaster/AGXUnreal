b671351a5f6e9375b607030fb8dc00b2366d3ae9
Josef Holmner <josef.holmner@algoryx.se>
2020-11-25 21:59:31 +0100

    Dont set transform twice

--- Source/AGXUnrealEditor/Private/AGX_ArchiveImporterHelper.cpp
+++ Source/AGXUnrealEditor/Private/AGX_ArchiveImporterHelper.cpp
@@ -458,20 +458,16 @@ namespace
 			Bodies.first, Bodies.second, false, false, false);
 		/// \todo Check for nullptr;
 
+		FAGX_ConstraintUtilities::SetupConstraintAsFrameDefiningSource(
+			Barrier, *Actor->GetConstraintComponent(), Bodies.first, Bodies.second);
+
 		/// \todo Make CopyControllersFrom a virtual member function of UAGX_ConstraintComponent.
 		/// Then we won't need the code duplication in the functions calling this one.
-
-		/// \todo Compute the inverse of the first body's attachment frame. That will place the
-		/// Actor at the right spot relative to that body.
-		Actor->SetActorTransform(Bodies.first->GetComponentTransform());
 
 		// This is where AttachToActor was. Now it's in the ToActorTree class.
 
 		FAGX_ImportUtilities::Rename(*Actor, Barrier.GetName());
 		/// \todo Should we call SetActorLabel here?
-
-		FAGX_ConstraintUtilities::SetupConstraintAsFrameDefiningSource(
-			Barrier, *Actor->GetConstraintComponent(), Bodies.first, Bodies.second);
 
 		return Actor;
 	}
