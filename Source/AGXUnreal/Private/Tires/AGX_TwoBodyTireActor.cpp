b0081c9c75ae9fd07172e7e4523885c6211b6ae4
Josef Holmner <josef.holmner@algoryx.se>
2020-12-01 20:20:27 +0100

    Add RigidBody components to TwoBodyTireActor and setup the TwoBodyTireComponent in constructor

--- Source/AGXUnreal/Private/Tires/AGX_TwoBodyTireActor.cpp
+++ Source/AGXUnreal/Private/Tires/AGX_TwoBodyTireActor.cpp
@@ -2,6 +2,7 @@
 
 // AGXUnreal includes.
 #include "AGX_TwoBodyTireComponent.h"
+#include "AGX_RigidBodyComponent.h"
 
 // Unreal Engine includes.
 #include "Engine/EngineTypes.h"
@@ -10,6 +11,24 @@ AAGX_TwoBodyTireActor::AAGX_TwoBodyTireActor()
 {
 	PrimaryActorTick.bCanEverTick = false;
 
+	Root =
+		CreateDefaultSubobject<USceneComponent>(TEXT("RootComponent"));
+	RootComponent = Root;
+
+	TireRigidBodyComponent =
+		CreateDefaultSubobject<UAGX_RigidBodyComponent>(TEXT("TireRigidBodyComponent"));
+	TireRigidBodyComponent->SetupAttachment(RootComponent);
+
+	HubRigidBodyComponent =
+		CreateDefaultSubobject<UAGX_RigidBodyComponent>(TEXT("HubRigidBodyComponent"));
+	HubRigidBodyComponent->SetupAttachment(RootComponent);
+
 	TwoBodyTireComponent =
 		CreateDefaultSubobject<UAGX_TwoBodyTireComponent>(TEXT("TwoBodyTireComponent"));
+
+	TwoBodyTireComponent->TireRigidBody.OwningActor = this;
+	TwoBodyTireComponent->TireRigidBody.BodyName = TireRigidBodyComponent->GetFName();
+
+	TwoBodyTireComponent->HubRigidBody.OwningActor = this;
+	TwoBodyTireComponent->HubRigidBody.BodyName = HubRigidBodyComponent->GetFName();
 }
--- Source/AGXUnreal/Public/Tires/AGX_TwoBodyTireActor.h
+++ Source/AGXUnreal/Public/Tires/AGX_TwoBodyTireActor.h
@@ -6,6 +6,7 @@
 #include "AGX_TwoBodyTireActor.generated.h"
 
 class UAGX_TwoBodyTireComponent;
+class UAGX_RigidBodyComponent;
 
 /**
  * An Actor that has an AGX_TwoBodyTireComponent.
@@ -21,6 +22,15 @@ class AGXUNREAL_API AAGX_TwoBodyTireActor : public AActor
 
 public:
 	AAGX_TwoBodyTireActor();
+
+	UPROPERTY(Category = "AGX Dynamics", VisibleAnywhere, BlueprintReadOnly)
+	USceneComponent* Root;
+
+	UPROPERTY(Category = "AGX Dynamics", VisibleAnywhere, BlueprintReadOnly)
+	UAGX_RigidBodyComponent* TireRigidBodyComponent;
+
+	UPROPERTY(Category = "AGX Dynamics", VisibleAnywhere, BlueprintReadOnly)
+	UAGX_RigidBodyComponent* HubRigidBodyComponent;
 
 	UPROPERTY(Category = "AGX Dynamics", VisibleAnywhere, BlueprintReadOnly)
 	UAGX_TwoBodyTireComponent* TwoBodyTireComponent;
