fd2e5c1629775d7a9602f933e205cee0b89ff4d8
Josef Holmner <josef.holmner@algoryx.se>
2020-12-08 15:23:42 +0100

    Format TwoBodyTireActor

--- Source/AGXUnreal/Private/Tires/AGX_TwoBodyTireActor.cpp
+++ Source/AGXUnreal/Private/Tires/AGX_TwoBodyTireActor.cpp
@@ -11,20 +11,16 @@ AAGX_TwoBodyTireActor::AAGX_TwoBodyTireActor()
 {
 	PrimaryActorTick.bCanEverTick = false;
 
-	Root =
-		CreateDefaultSubobject<USceneComponent>(TEXT("Root"));
+	Root = CreateDefaultSubobject<USceneComponent>(TEXT("Root"));
 	RootComponent = Root;
 
-	TireRigidBodyComponent =
-		CreateDefaultSubobject<UAGX_RigidBodyComponent>(TEXT("TireRigidBody"));
+	TireRigidBodyComponent = CreateDefaultSubobject<UAGX_RigidBodyComponent>(TEXT("TireRigidBody"));
 	TireRigidBodyComponent->SetupAttachment(RootComponent);
 
-	HubRigidBodyComponent =
-		CreateDefaultSubobject<UAGX_RigidBodyComponent>(TEXT("HubRigidBody"));
+	HubRigidBodyComponent = CreateDefaultSubobject<UAGX_RigidBodyComponent>(TEXT("HubRigidBody"));
 	HubRigidBodyComponent->SetupAttachment(RootComponent);
 
-	TwoBodyTireComponent =
-		CreateDefaultSubobject<UAGX_TwoBodyTireComponent>(TEXT("TwoBodyTire"));
+	TwoBodyTireComponent = CreateDefaultSubobject<UAGX_TwoBodyTireComponent>(TEXT("TwoBodyTire"));
 
 	TwoBodyTireComponent->TireRigidBody.OwningActor = this;
 	TwoBodyTireComponent->TireRigidBody.BodyName = TireRigidBodyComponent->GetFName();
