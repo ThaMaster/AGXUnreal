da46205cc67edafa82bd863c7f140163a0d48ca8
Josef Holmner <josef.holmner@algoryx.se>
2020-12-28 20:13:14 +0100

    Remove check is license checked

--- Source/AGXUnreal/Private/AGX_Simulation.cpp
+++ Source/AGXUnreal/Private/AGX_Simulation.cpp
@@ -241,8 +241,6 @@ void UAGX_Simulation::Step(float DeltaTime)
 	}
 #endif
 
-	check(IsLicenseChecked);
-
 	TRACE_CPUPROFILER_EVENT_SCOPE(TEXT("AGXUnreal:UAGX_Simulation::Step"));
 	switch (StepMode)
 	{
