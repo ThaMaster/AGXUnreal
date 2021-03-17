45ecc06204e5824ba2e5b78c31de98fe2cd19b25
Martin Nilsson <martin.nilsson@algoryx.se>
2021-03-17 18:27:40 +0100

    Explicitly fully load Terrain Material Library asset on creation

--- Source/AGXUnrealEditor/Private/Materials/AGX_TerrainMaterialLibrary.cpp
+++ Source/AGXUnrealEditor/Private/Materials/AGX_TerrainMaterialLibrary.cpp
@@ -29,10 +29,7 @@ namespace AGX_TerrainMaterialLibrary_helpers
 			FString::Printf(TEXT("/AGXUnreal/Terrain/TerrainMaterialLibrary/%s"), *AssetName);
 		UPackage* Package = CreatePackage(nullptr, *PackagePath);
 
-// Not sure if or when this is needed or legal.
-#if 0
 		Package->FullyLoad();
-#endif
 
 		// Create the asset itself, reading data from the AGX Dynamics terrain material library.
 		FTerrainMaterialBarrier Material =
@@ -47,6 +44,7 @@ namespace AGX_TerrainMaterialLibrary_helpers
 		Asset->PostEditChange();
 		Asset->AddToRoot();
 		Package->SetDirtyFlag(true);
+		Package->FullyLoad();
 		const FString PackageFilename = FPackageName::LongPackageNameToFilename(
 			PackagePath, FPackageName::GetAssetPackageExtension());
 		Package->GetMetaData();
@@ -59,6 +57,15 @@ namespace AGX_TerrainMaterialLibrary_helpers
 				TEXT("Could not create terrain library material %s: UPackage::SavePackage failed."),
 				*NameAGX);
 		}
+
+		// Must fully load the package or else project packaging will fail with:
+		//
+		//    Package /AGXUnreal/Terrain/TerrainMaterialLibrary/AGX_TM_gravel_1 supposed
+		//    to be fully loaded but isn't. RF_WasLoaded is set
+		//
+		//    Unable to cook package for platform because it is unable to be loaded:
+		//    <PATH>/AGXUnreal/Content/Terrain/TerrainMaterialLibrary/AGX_TM_gravel_1.uasset
+		Package->FullyLoad();
 	}
 }
 
