ec5283db9d9bfa676a727f067070ef0acd1c1016
Josef Holmner <josef.holmner@algoryx.se>
2021-04-29 15:01:26 +0200

    Use AsyncLandscapeSampler in HeightFieldUtilities and get heights using multithreading

--- Source/AGXUnreal/Private/Utilities/AGX_HeightFieldUtilities.cpp
+++ Source/AGXUnreal/Private/Utilities/AGX_HeightFieldUtilities.cpp
@@ -3,151 +3,73 @@
 // AGX Dynamics for Unreal includes.
 #include "AGX_LogCategory.h"
 #include "Terrain/AGX_LandscapeSizeInfo.h"
+#include "Terrain/AGX_AsyncLandscapeSampler.h"
 
 // Unreal Engine includes.
 #include "Landscape.h"
-#include "LandscapeDataAccess.h"
-#include "LandscapeComponent.h"
 
 #include <limits>
 
 namespace
 {
-	// Nudge point away from the edge of the landscape if the vertex lies at the edge.
-	// By setting ForceNudge = true the point will be nudged even if it does not lie at the
-	// landscape edge.
-	inline void NudgeEdgePoint(
-		const int32 VertX, const int32 VertY, const FAGX_LandscapeSizeInfo& LandscapeSizeInfo,
-		float& PointX, float& PointY, bool ForceNudge)
-	{
-		const float NudgeDistX = LandscapeSizeInfo.QuadSideSizeX / 1000.0f;
-		const float NudgeDistY = LandscapeSizeInfo.QuadSideSizeY / 1000.0f;
-		const int32 EdgeVertexX = LandscapeSizeInfo.NumVerticesSideX - 1;
-		const int32 EdgeVertexY = LandscapeSizeInfo.NumVerticesSideY - 1;
-
-		if (VertX == 0)
-		{
-			PointX += NudgeDistX;
-		}
-		else if (VertX == EdgeVertexX || ForceNudge)
-		{
-			PointX -= NudgeDistX;
-		}
-
-		if (VertY == 0)
-		{
-			PointY += NudgeDistY;
-		}
-		else if (VertY == EdgeVertexY || ForceNudge)
-		{
-			PointY -= NudgeDistY;
-		}
-	}
-
-	// Shoot single ray at landscape to measure the height. Returns false if the ray misses the
-	// landscape and true otherwise. If it returns false the OutHeight is set to 0.0 but is not a
-	// valid measurement.
-	inline bool ShootSingleRay(
-		ALandscape& Landscape, const int32 VertX, const int32 VertY, const float ZOffsetLocal,
-		const FAGX_LandscapeSizeInfo& LandscapeSizeInfo,
-		const FCollisionQueryParams& CollisionParams, FHitResult& HitResult, float& OutHeight,
-		bool ForceNudge = false)
-	{
-		OutHeight = 0.0f;
-
-		// Vertex position in the landscapes local coordinate system.
-		float Xlocal = VertX * LandscapeSizeInfo.QuadSideSizeX;
-		float Ylocal = VertY * LandscapeSizeInfo.QuadSideSizeY;
-
-		// The line trace will not detect a hit if it goes through a vertex at the edge of
-		// the landscape (might be a floating point precision issue). We fix this by nudging
-		// any point at the edge away from the edge and inwards slightly. If the ForceNudge
-		// parameter is set to true, the point will be nudged even if it is not on the edge.
-		NudgeEdgePoint(VertX, VertY, LandscapeSizeInfo, Xlocal, Ylocal, ForceNudge);
-
-		// Ray start and end positions in global coordinates.
-		const FVector RayStart = Landscape.GetTransform().TransformPositionNoScale(
-			FVector(Xlocal, Ylocal, ZOffsetLocal));
-		const FVector RayEnd = Landscape.GetTransform().TransformPositionNoScale(
-			FVector(Xlocal, Ylocal, -ZOffsetLocal));
-
-		if (Landscape.ActorLineTraceSingle(
-				HitResult, RayStart, RayEnd, ECC_Visibility, CollisionParams))
-		{
-			// The hit point of the line trace in the landscape's local coordinate system.
-			FVector ImpactPointLocal =
-				Landscape.GetTransform().InverseTransformPositionNoScale(HitResult.ImpactPoint);
-
-			OutHeight = ImpactPointLocal.Z;
-			return true;
-		}
-
-		// Line trace missed.
-		return false;
-	}
-
 	TArray<float> GetHeights(ALandscape& Landscape, const FAGX_LandscapeSizeInfo& LandscapeSizeInfo)
 	{
-		TArray<float> Heights;
-		Heights.AddUninitialized(LandscapeSizeInfo.NumVertices);
+		// One thread per Y-side vertex, but at most 4 threads.
+		const int32 NumThreads = FMath::Clamp(LandscapeSizeInfo.NumVerticesSideY, 1, 4);
 
-		// Line traces will be used to measure the heights of the landscape.
-		const FCollisionQueryParams CollisionParams(FName(TEXT("LandscapeHeightFieldTrace")));
-		FHitResult HitResult(ForceInit);
+		// Split the Landscape up between the threads so that each thread gets an equal range of
+		// Y-side vertices, except for the last thread that also gets any remainder from the
+		// division. This way of splitting up the Landscape ensures that the height data is stored
+		// in the correct order when finally concatenating the TArrays that the threads produced.
+		const int32 YVertsPerThread =
+			LandscapeSizeInfo.NumVerticesSideY / NumThreads; // Truncates down to nearest integer.
+		const FAGX_AsyncLandscapeSampler::VertexSpan SpanX(0, LandscapeSizeInfo.NumVerticesSideX);
+		FAGX_AsyncLandscapeSampler::VertexSpan SpanY(
+			LandscapeSizeInfo.NumVerticesSideY - YVertsPerThread,
+			LandscapeSizeInfo.NumVerticesSideY);
 
-		// At scale = 1, the height span is +- 256 cm
-		// https://docs.unrealengine.com/en-US/Engine/Landscape/TechnicalGuide/#calculatingheightmapzscale
-		const float HEIGHT_SPAN_HALF = 256.0f * LandscapeSizeInfo.LandscapeScaleZ;
-
-		int32 Vertex = 0;
-		int32 LineTraceMisses = 0;
-		// AGX terrains Y axis goes in the opposite direction from Unreals Y axis (flipped).
-		for (int32 Y = LandscapeSizeInfo.NumVerticesSideY - 1; Y >= 0; Y--)
+		TArray<FAGX_AsyncLandscapeSampler> Samplers;
+		for (int32 i = 0; i < NumThreads; i++)
 		{
-			for (int32 X = 0; X < LandscapeSizeInfo.NumVerticesSideX; X++)
+			if (i == NumThreads - 1)
 			{
-				float Height = 0.0f;
-
-				// Use line trace to read the landscape height for this vertex.
-				if (ShootSingleRay(
-						Landscape, X, Y, HEIGHT_SPAN_HALF, LandscapeSizeInfo, CollisionParams,
-						HitResult, Height))
-				{
-					Heights[Vertex] = Height;
-				}
-
-				// Line trace missed. This is unusual but has been observed with large
-				// landscapes at the seams between landscape components/sections, similar to
-				// line traces at the very edge being missed. Re-try the line trace but force
-				// the ray's intersection point to be nudged slightly.
-				else if (ShootSingleRay(
-							 Landscape, X, Y, HEIGHT_SPAN_HALF, LandscapeSizeInfo, CollisionParams,
-							 HitResult, Height, true))
-				{
-					Heights[Vertex] = Height;
-				}
-				else
-				{
-					// Line trace missed even after force nudge, which is unexpected. We will log a
-					// warning and set the height value for this vertex to 0.0 and continue. If this
-					// happens rarely enough the results may still be useful.
-					LineTraceMisses++;
-					Heights[Vertex] = 0.0;
-				}
-
-				Vertex++;
+				// The last Sampler gets any remainder from the division of Y-side vertices among
+				// the threads, i.e. it's SpanY always goes from 0.
+				Samplers.Add(FAGX_AsyncLandscapeSampler(
+					Landscape, LandscapeSizeInfo, SpanX, {0, SpanY.End}));
 			}
+			else
+			{
+				Samplers.Add(
+					FAGX_AsyncLandscapeSampler(Landscape, LandscapeSizeInfo, SpanX, SpanY));
+			}
+
+			SpanY = SpanY - YVertsPerThread;
 		}
 
-		check(Vertex == LandscapeSizeInfo.NumVertices);
+		for (auto& Sampler : Samplers)
+		{
+			Sampler.StartAsync();
+		}
 
+		TArray<float> Heights;
+		Heights.Reserve(LandscapeSizeInfo.NumVertices);
+		int32 LineTraceMisses = 0;
+		for (auto& Sampler : Samplers)
+		{
+			Sampler.Join();
+			Heights.Append(Sampler.GetHeights());
+			LineTraceMisses += Sampler.GetNumLineTraceMisses();
+		}
+
+		check(Heights.Num() == LandscapeSizeInfo.NumVertices);
 		if (LineTraceMisses > 0)
 		{
 			UE_LOG(
 				LogAGX, Warning,
 				TEXT("%d of %d vertices could not be read from the landscape. The heights of the "
 					 "coresponding vertices in the AGX Terrain may therefore be incorrect."),
-				LineTraceMisses, Vertex);
+				LineTraceMisses, LandscapeSizeInfo.NumVertices);
 		}
 
 		return Heights;
@@ -170,7 +92,6 @@ FHeightFieldShapeBarrier AGX_HeightFieldUtilities::CreateHeightField(ALandscape&
 	}
 
 	TArray<float> Heights = GetHeights(Landscape, LandscapeSizeInfo);
-
 	const float SideSizeX = LandscapeSizeInfo.NumQuadsSideX * LandscapeSizeInfo.QuadSideSizeX;
 	const float SideSizeY = LandscapeSizeInfo.NumQuadsSideY * LandscapeSizeInfo.QuadSideSizeY;
 
