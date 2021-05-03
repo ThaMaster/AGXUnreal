8fd01757596e0b19b2e217783d325bf12b0029ec
Martin Nilsson <martin.nilsson@algoryx.se>
2021-05-03 11:26:49 +0200

    Add documentation comments to Wire Node

--- Source/AGXUnreal/Public/Wire/AGX_WireNode.h
+++ Source/AGXUnreal/Public/Wire/AGX_WireNode.h
@@ -9,6 +9,12 @@
 
 #include "AGX_WireNode.generated.h"
 
+/**
+ * A FAGX_WireNode is a handle to an AGX Dynamics Wire Node. It does not own the underlying node,
+ * the Wire does that, and since wire nodes are created and destroyed throughout the simulation it
+ * is safest to re-fetch the nodes each frame through the Wire Component. Some nodes, such as Eye
+ * and Body Fixed, are persistent and guaranteed to not be removed by the wire resolution updates.
+ */
 USTRUCT(BlueprintType, Category = "AGX Dynamics")
 struct AGXUNREAL_API FAGX_WireNode
 {
@@ -21,7 +27,10 @@ public:
 
 	FAGX_WireNode& operator=(const FAGX_WireNode& InOther);
 
+	/// @return The world location of this node.
 	FVector GetWorldLocation() const;
+
+	/// @return The type of this node.
 	EWireNodeType GetType() const;
 
 private:
