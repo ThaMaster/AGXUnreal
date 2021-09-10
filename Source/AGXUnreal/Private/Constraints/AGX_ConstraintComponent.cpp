7923335aff1378d4ec4af1b8f289a40a2bd74205
Martin Nilsson <martin.nilsson@algoryx.se>
2021-09-10 12:56:23 +0200

    Reformulate late native restore during Blueprint Reconstruction error message

--- Source/AGXUnreal/Private/Constraints/AGX_ConstraintComponent.cpp
+++ Source/AGXUnreal/Private/Constraints/AGX_ConstraintComponent.cpp
@@ -452,13 +452,27 @@ FConstraintBarrier* UAGX_ConstraintComponent::GetOrCreateNative()
 {
 	if (!HasNative())
 	{
-		checkf(
-			!GIsReconstructingBlueprintInstances,
-			TEXT("This is a bad situation. Someone need this Component's native but we're in the "
-				 "middle of a RerunConstructionScripts and this Component haven't been given its "
-				 "Native yet. We can't create a new one since we will be given the actual Native "
-				 "soon, but we also can't return the actual Native right now because it hasn't "
-				 "been restored from the UActorComponentInstanceData yet."));
+		if (GIsReconstructingBlueprintInstances)
+		{
+			// We're in a very bad situation. Someone need this Component's native but if we're in
+			// the middle of a RerunConstructionScripts and this Component haven't been given its
+			// Native yet then there isn't much we can do. We can't create a new one since we will
+			// be given the actual Native soon, but we also can't return the actual Native right now
+			// because it hasn't been restored from the Component Instance Data yet.
+			//
+			// For now we simply die in non-shipping (checkNoEntry is active) so unit tests will
+			// detect this situation, and log error and return nullptr otherwise, so that the
+			// application can at least keep running. It is unlikely that the simulation will behave
+			// as intended.
+			checkNoEntry();
+			UE_LOG(
+				LogAGX, Error,
+				TEXT("A request for the AGX Dynamics instance for Constraint '%s' in '%s' was made "
+					 "but we are in the middle of a Blueprint Reconstruction and the requested "
+					 "instance has not yet been restored. The instance cannot be returned, which "
+					 "may lead to incorrect scene configuration."));
+			return nullptr;
+		}
 
 		CreateNative();
 	}
