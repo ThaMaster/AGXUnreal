2c3173bd5d16e92fc64ed7d2e9c1141e7917a1ee
Martin Nilsson <martin.nilsson@algoryx.se>
2021-09-22 09:38:35 +0200

    Only trigger the Property Dispatcher from the Chain property changed callback in constraints
    Because the Chain variant calls the non-chain variant and it's not
    necessary to trigger the Property Dispatcher twice.

--- Source/AGXUnreal/Private/Constraints/AGX_Constraint1DofComponent.cpp
+++ Source/AGXUnreal/Private/Constraints/AGX_Constraint1DofComponent.cpp
@@ -67,26 +67,10 @@ void UAGX_Constraint1DofComponent::PostInitProperties()
 		GET_MEMBER_NAME_CHECKED(ThisClass, TargetSpeedController));
 }
 
-void UAGX_Constraint1DofComponent::PostEditChangeProperty(
-	FPropertyChangedEvent& PropertyChangedEvent)
-{
-	PropertyDispatcher.Trigger(PropertyChangedEvent, this);
-
-	// If we are part of a Blueprint then this will trigger a RerunConstructionScript on the owning
-	// Actor. That means that his object will be removed from the Actor and destroyed. We want to
-	// apply all our changes before that so that they are carried over to the copy.
-	Super::PostEditChangeProperty(PropertyChangedEvent);
-}
-
 void UAGX_Constraint1DofComponent::PostEditChangeChainProperty(
 	struct FPropertyChangedChainEvent& PropertyChangedEvent)
 {
-	if (PropertyChangedEvent.PropertyChain.Num() > 2)
-	{
-		// The cases fewer chain elements are handled by PostEditChangeProperty, which is called by
-		// UObject's PostEditChangeChainProperty.
-		PropertyDispatcher.Trigger(PropertyChangedEvent, this);
-	}
+	PropertyDispatcher.Trigger(PropertyChangedEvent, this);
 
 	// If we are part of a Blueprint then this will trigger a RerunConstructionScript on the owning
 	// Actor. That means that this object will be removed from the Actor and destroyed. We want to
