38a5c08bba1ab0fe046093a53b5dc8b4df6bee89
Josef Holmner <josef.holmner@algoryx.se>
2021-10-04 09:10:53 +0200

    Work must add up to AmountOfWork

--- Source/AGXUnrealBarrier/Private/AGXSimObjectsReader.cpp
+++ Source/AGXUnrealBarrier/Private/AGXSimObjectsReader.cpp
@@ -402,7 +402,7 @@ namespace
 		MyTask.EnterProgressFrame(1.0f, FText::FromString("Importing Materials"));
 		Result &= ReadMaterials(Simulation, Instantiator);
 
-		MyTask.EnterProgressFrame(5.0f, FText::FromString("Importing Tire Models"));
+		MyTask.EnterProgressFrame(1.0f, FText::FromString("Importing Tire Models"));
 		Result &= ReadTireModels(Simulation, Filename, Instantiator);
 
 		MyTask.EnterProgressFrame(15.0f, FText::FromString("Importing Rigid Bodies and Geometries"));
@@ -411,13 +411,13 @@ namespace
 		MyTask.EnterProgressFrame(80.0f, FText::FromString("Importing bodiless Geometries"));
 		Result &= ReadBodilessGeometries(Simulation, Filename, Instantiator);
 
-		MyTask.EnterProgressFrame(90.0f, FText::FromString("Importing Constraints"));
+		MyTask.EnterProgressFrame(1.0f, FText::FromString("Importing Constraints"));
 		Result &= ReadConstraints(Simulation, Filename, Instantiator);
 
-		MyTask.EnterProgressFrame(92.0f, FText::FromString("Importing Collision Groups"));
+		MyTask.EnterProgressFrame(1.0f, FText::FromString("Importing Collision Groups"));
 		Result &= ReadCollisionGroups(Simulation, Instantiator);
 
-		MyTask.EnterProgressFrame(95.0f, FText::FromString("Importing Wires"));
+		MyTask.EnterProgressFrame(1.0f, FText::FromString("Importing Wires"));
 		Result &= ReadWires(Simulation, Filename, Instantiator);
 
 		return Result;
