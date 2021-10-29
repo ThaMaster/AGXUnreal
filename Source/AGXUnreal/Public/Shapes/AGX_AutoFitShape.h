2efedea6249def8b82993b5328ed9b3f8d8240fb
Josef Holmner <josef.holmner@algoryx.se>
2021-10-29 10:21:30 +0200

    Add AutofitShape

---
+++ Source/AGXUnreal/Public/Shapes/AGX_AutoFitShape.h
@@ -0,0 +1,27 @@
+#pragma once
+
+// AGX Dynamics for Unreal includes.
+#include "AGX_MeshWithTransform.h"
+
+// Unreal Engine includes.
+#include "CoreMinimal.h"
+
+class UWorld;
+
+class AGXUNREAL_API AGX_AutoFitShape
+{
+public:
+	/*
+	 * Auto-fits to the collection of FAGX_MeshWithTransforms.
+	 */
+	bool AutoFit(TArray<FAGX_MeshWithTransform> Meshes, UWorld* World, const FString& ShapeName);
+
+	/*
+	 * Auto-fits to the collection of UStaticMeshComponen Children. Ensures the original world transform
+	 * of the Children is preserved.
+	 */
+	bool AutoFitToChildren(
+		TArray<UStaticMeshComponent*> Children, UWorld* World, const FString& ShapeName);
+
+	virtual bool AutoFitFromVertices(const TArray<FVector>& Vertices) = 0;
+};
