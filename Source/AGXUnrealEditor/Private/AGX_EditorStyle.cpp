11d26b61b7224e89b5cd569587d3abc0a6c1d6d6
Martin Nilsson <martin.nilsson@algoryx.se>
2021-11-02 16:01:06 +0100

    Add Component icons for a small set of Components

--- Source/AGXUnrealEditor/Private/AGX_EditorStyle.cpp
+++ Source/AGXUnrealEditor/Private/AGX_EditorStyle.cpp
@@ -76,6 +76,11 @@ TSharedRef<class FSlateStyleSet> FAGX_EditorStyle::Create()
 
 	Style->Set(AgxIcon, new IMAGE_BRUSH("Icons/T_AgxIcon47", IconSize40));
 	Style->Set(AgxIconSmall, new IMAGE_BRUSH("Icons/T_AgxIcon47", IconSize16));
+	Style->Set("ClassIcon.AGX_RigidBodyComponent", new IMAGE_BRUSH("Icons/T_AgxIcon47", IconSize16));
+	Style->Set("ClassIcon.AGX_RigidBodyActor", new IMAGE_BRUSH("Icons/T_AgxIcon47", IconSize16));
+	Style->Set("ClassIcon.AGX_BoxShapeComponent", new IMAGE_BRUSH("Icons/T_BoxIcon", IconSize16));
+	Style->Set("ClassIcon.AGX_CylinderShapeComponent", new IMAGE_BRUSH("Icons/T_CylinderIcon", IconSize16));
+	Style->Set("ClassIcon.AGX_CapsuleShapeComponent", new IMAGE_BRUSH("Icons/T_CapsuleIcon", IconSize16));
 
 	return Style;
 };
