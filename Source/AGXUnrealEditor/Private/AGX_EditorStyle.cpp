0194f34391a73d3252ebbe47209028daced6f961
Martin Nilsson <martin.nilsson@algoryx.se>
2021-11-03 08:05:19 +0100

    Rename Joint icon to Constraint
    For consistency with how constraints are named in the plugin.

--- Source/AGXUnrealEditor/Private/AGX_EditorStyle.cpp
+++ Source/AGXUnrealEditor/Private/AGX_EditorStyle.cpp
@@ -78,8 +78,8 @@ TSharedRef<class FSlateStyleSet> FAGX_EditorStyle::Create()
 
 	Style->Set(AgxIcon, new IMAGE_BRUSH("Icons/T_Icon_A_Light_776", IconSize40));
 	Style->Set(AgxIconSmall, new IMAGE_BRUSH("Icons/T_Icon_A_Light_776", IconSize16));
-	Style->Set(JointIcon, new IMAGE_BRUSH("Icons/T_Icon_Joint_200", IconSize40));
-	Style->Set(JointIconSmall, new IMAGE_BRUSH("Icons/T_Icon_Joint_200", IconSize16));
+	Style->Set(JointIcon, new IMAGE_BRUSH("Icons/T_Icon_Constraint_200", IconSize40));
+	Style->Set(JointIconSmall, new IMAGE_BRUSH("Icons/T_Icon_Constraint_200", IconSize16));
 	Style->Set("ClassIcon.AGX_RigidBodyComponent", new IMAGE_BRUSH("Icons/T_Icon_A_Light_776", IconSize16));
 	Style->Set("ClassIcon.AGX_RigidBodyActor", new IMAGE_BRUSH("Icons/T_Icon_A_Light_776", IconSize16));
 	Style->Set("ClassIcon.AGX_BoxShapeComponent", new IMAGE_BRUSH("Icons/T_Icon_Box_200", IconSize16));
