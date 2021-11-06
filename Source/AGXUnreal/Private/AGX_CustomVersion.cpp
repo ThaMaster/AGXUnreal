2a66333ec52cb80fd11faf456091824a370ce413
Josef Holmner <josef.holmner@algoryx.se>
2021-11-06 13:34:36 +0100

    Add more clear comment about backward compatibility reset

--- Source/AGXUnreal/Private/AGX_CustomVersion.cpp
+++ Source/AGXUnreal/Private/AGX_CustomVersion.cpp
@@ -3,6 +3,12 @@
 // Unreal Engine includes.
 #include "Serialization/CustomVersion.h"
 
+// Important: Do not change the value of this GUID. Doing so will break any backward compatibility
+// for previous AGX Dynamics for Unreal releases. There is an exception: if a backward
+// compatibility "reset" is wanted, for example when moving to a new major release of the plugin,
+// this GUID must be changed to a new and randomly generated GUID. When doing such a "reset", the
+// enum literals in FAGX_CustomVersion::Type should be removed also. See the comment in
+// AGX_CusomVersion.h for more details.
 const FGuid FAGX_CustomVersion::GUID(0x9A157FFA, 0x909A4B69, 0xBE1B12A0, 0x51B6F233);
 
 // Register the custom version with core
--- Source/AGXUnreal/Public/AGX_CustomVersion.h
+++ Source/AGXUnreal/Public/AGX_CustomVersion.h
@@ -8,6 +8,11 @@ struct FAGX_CustomVersion
 {
 	// Important: Do not remove or change the order of enum literals if those have been released
 	// publicly. Doing so will break backward compatibility for anyone using that release.
+	// There is an exception: if a backwards compatibility "reset" is wanted, for example when
+	// moving to a new major release, all enum literals in this Type enum except the first and the
+	// two last ones can be removed. When doing such a "reset", remember to set a newly randomly
+	// generated GUID in AGX_CustomVersion.cpp also. That GUID together with these enum literal
+	// values are what makes a certain version completely unique.
 	enum Type
 	{
 		// Before any version changes were made.
