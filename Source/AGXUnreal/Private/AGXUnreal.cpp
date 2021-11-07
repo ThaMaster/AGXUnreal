6e9357999082f9d3d600163c09fb47b4ef13387f
Josef Holmner <josef.holmner@algoryx.se>
2021-11-07 21:35:03 +0100

    Add Core Redirects empty function for future reference

--- Source/AGXUnreal/Private/AGXUnreal.cpp
+++ Source/AGXUnreal/Private/AGXUnreal.cpp
@@ -1,5 +1,6 @@
 #include "AGXUnreal.h"
 
+#include "UObject/CoreRedirects.h"
 #include "AGX_LogCategory.h"
 #include "AGX_RuntimeStyle.h"
 
@@ -9,6 +10,7 @@ void FAGXUnrealModule::StartupModule()
 {
 	UE_LOG(LogAGX, Log, TEXT("FAGXUnrealModule::StartupModule()"));
 
+	RegisterCoreRedirects();
 	FAGX_RuntimeStyle::Initialize();
 	FAGX_RuntimeStyle::ReloadTextures();
 }
@@ -18,6 +20,24 @@ void FAGXUnrealModule::ShutdownModule()
 	UE_LOG(LogAGX, Log, TEXT("FAGXUnrealModule::ShutdownModule()"));
 
 	FAGX_RuntimeStyle::Shutdown();
+}
+
+void FAGXUnrealModule::RegisterCoreRedirects()
+{
+	// This is used to handle name changes of UFUNCTIONs, UPROPERTYs and UObjects (backward
+	// compatibility).
+
+	// Uncomment below once the first Core Redirect is added. Remove this comment when that happens.
+
+	/*
+	TArray<FCoreRedirect> Redirects;
+
+	Redirects.Emplace(
+		ECoreRedirectFlags::Type_Property, TEXT("AGX_ExampleComponent.OldExampleProperty"),
+		TEXT("NewExampleProperty"));
+
+	FCoreRedirects::AddRedirectList(Redirects, TEXT("AGXUnreal"));
+	*/
 }
 
 #undef LOCTEXT_NAMESPACE
