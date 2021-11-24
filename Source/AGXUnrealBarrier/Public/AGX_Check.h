8fb33e14cc75dce7bea36045845478ce64023fd1
Martin Nilsson <martin.nilsson@algoryx.se>
2021-11-24 15:32:05 +0100

    Set AGXUNREAL_CHECK in .gitlab-ci.yml for unit test jobs instead of in the build script
    In case we want to use the build script for other purposes

--- Source/AGXUnrealBarrier/Public/AGX_Check.h
+++ Source/AGXUnrealBarrier/Public/AGX_Check.h
@@ -1,7 +1,9 @@
 #pragma once
 
 #if defined(AGXUNREAL_CHECK) && AGXUNREAL_CHECK == 1
+#	pragma message("AGX_CHECK enabled.")
 #	define AGX_CHECK(x) check(x)
 #else
+#	pragma message("AGX_CHECK disabled.")
 #	define AGX_CHECK(x)
 #endif
