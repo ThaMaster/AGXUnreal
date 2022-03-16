72624980c9bc7815c6ff7c3e746daae351d49e3e
Josef Holmner <josef.holmner@algoryx.se>
2022-03-16 08:00:36 +0100

    Add missing include directive

--- Source/AGXUnrealEditor/Private/Wire/AGX_WireNodeDetails.cpp
+++ Source/AGXUnrealEditor/Private/Wire/AGX_WireNodeDetails.cpp
@@ -12,6 +12,7 @@
 // Unreal Engine includes.
 #include "DetailLayoutBuilder.h"
 #include "DetailWidgetRow.h"
+#include "Editor/UnrealEdEngine.h"
 #include "IDetailChildrenBuilder.h"
 #include "IDetailGroup.h"
 #include "Misc/Attribute.h"
