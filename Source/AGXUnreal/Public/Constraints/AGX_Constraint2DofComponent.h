a1f842df8113ab19d432434a0f6a9e3b48f86b62
Martin Nilsson <martin.nilsson@algoryx.se>
2022-04-04 15:58:50 +0200

    Expose the Secondary Constraints in Constraint2Dof to Blueprint Visual Scripts

--- Source/AGXUnreal/Public/Constraints/AGX_Constraint2DofComponent.h
+++ Source/AGXUnreal/Public/Constraints/AGX_Constraint2DofComponent.h
@@ -28,50 +28,50 @@ class AGXUNREAL_API UAGX_Constraint2DofComponent : public UAGX_ConstraintCompone
 public:
 	/** Electric motor controller for first secondary constraint (on one of the 2 free DOFs,
 	 * usually). */
-	UPROPERTY(EditAnywhere, Category = "AGX Secondary Constraints")
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "AGX Secondary Constraints")
 	FAGX_ConstraintElectricMotorController ElectricMotorController1;
 
 	/** Electric motor controller for second secondary constraint (on one of the 2 free DOFs,
 	 * usually). */
-	UPROPERTY(EditAnywhere, Category = "AGX Secondary Constraints")
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "AGX Secondary Constraints")
 	FAGX_ConstraintElectricMotorController ElectricMotorController2;
 
 	/** Friction controller for first secondary constraint (on one of the 2 free DOFs, usually). */
-	UPROPERTY(EditAnywhere, Category = "AGX Secondary Constraints")
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "AGX Secondary Constraints")
 	FAGX_ConstraintFrictionController FrictionController1;
 
 	/** Friction controller for second secondary constraint (on one of the 2 free DOFs, usually). */
-	UPROPERTY(EditAnywhere, Category = "AGX Secondary Constraints")
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "AGX Secondary Constraints")
 	FAGX_ConstraintFrictionController FrictionController2;
 
 	/** Lock controller for first secondary constraint (on one of the 2 free DOFs, usually). */
-	UPROPERTY(EditAnywhere, Category = "AGX Secondary Constraints")
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "AGX Secondary Constraints")
 	FAGX_ConstraintLockController LockController1;
 
 	/** Lock controller for second secondary constraint (on one of the 2 free DOFs, usually). */
-	UPROPERTY(EditAnywhere, Category = "AGX Secondary Constraints")
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "AGX Secondary Constraints")
 	FAGX_ConstraintLockController LockController2;
 
 	/** Range controller for first secondary constraint (on one of the 2 free DOFs, usually). */
-	UPROPERTY(EditAnywhere, Category = "AGX Secondary Constraints")
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "AGX Secondary Constraints")
 	FAGX_ConstraintRangeController RangeController1;
 
 	/** Range controller for second secondary constraint (on one of the 2 free DOFs, usually). */
-	UPROPERTY(EditAnywhere, Category = "AGX Secondary Constraints")
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "AGX Secondary Constraints")
 	FAGX_ConstraintRangeController RangeController2;
 
 	/** Target speed controller for first secondary constraint (on one of the 2 free DOFs, usually).
 	 */
-	UPROPERTY(EditAnywhere, Category = "AGX Secondary Constraints")
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "AGX Secondary Constraints")
 	FAGX_ConstraintTargetSpeedController TargetSpeedController1;
 
 	/** Target speed controller for second secondary constraint (on one of the 2 free DOFs,
 	 * usually). */
-	UPROPERTY(EditAnywhere, Category = "AGX Secondary Constraints")
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "AGX Secondary Constraints")
 	FAGX_ConstraintTargetSpeedController TargetSpeedController2;
 
 	/** Screw controller that puts a relationship between the two free DOFs. */
-	UPROPERTY(EditAnywhere, Category = "AGX Secondary Constraints")
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "AGX Secondary Constraints")
 	FAGX_ConstraintScrewController ScrewController;
 
 	UAGX_Constraint2DofComponent();
