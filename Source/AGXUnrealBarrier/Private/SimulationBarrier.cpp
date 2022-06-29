a7853ce1a9622ddc9aa15ff03ea47b3a75c7dcaf
Josef Holmner <josef.holmner@algoryx.se>
2022-06-29 08:55:59 +0200

    Temporarily comment out amor include

--- Source/AGXUnrealBarrier/Private/SimulationBarrier.cpp
+++ Source/AGXUnrealBarrier/Private/SimulationBarrier.cpp
@@ -22,7 +22,7 @@
 #include <agx/PointGravityField.h>
 #include <agx/Statistics.h>
 #include <agx/UniformGravityField.h>
-#include <agxSDK/MergeSplitHandler.h>
+//#include <agxSDK/MergeSplitHandler.h>
 #include <agxSDK/Simulation.h>
 #include "EndAGXIncludes.h"
 
@@ -189,7 +189,7 @@ void FSimulationBarrier::EnableRemoteDebugging(int16 Port)
 void FSimulationBarrier::SetEnableAmor(bool bEnable)
 {
 	check(HasNative());
-	NativeRef->Native->getMergeSplitHandler()->setEnable(bEnable);
+	//NativeRef->Native->getMergeSplitHandler()->setEnable(bEnable);
 }
 
 void FSimulationBarrier::SetTimeStep(float TimeStep)
