ff2e27afdbef4d84f7bfa063a6e3677692745ddf
Josef Holmner <josef.holmner@algoryx.se>
2022-09-02 15:55:17 +0200

    Move the redirects to an ini file

--- Source/AGXUnreal/Private/AGXUnreal.cpp
+++ Source/AGXUnreal/Private/AGXUnreal.cpp
@@ -30,30 +30,17 @@ void FAGXUnrealModule::RegisterCoreRedirects()
 	// compatibility). See
 	// https://docs.unrealengine.com/4.27/en-US/ProgrammingAndScripting/ProgrammingWithCPP/Assets/CoreRedirects/
 
+	// Uncomment below once the first Core Redirect is added. Remove this comment when that happens.
+
+	/*
 	TArray<FCoreRedirect> Redirects;
 
-	// Shape materials.
 	Redirects.Emplace(
-		ECoreRedirectFlags::Type_Class, TEXT("AGX_ShapeMaterialBase"), TEXT("AGX_ShapeMaterial"));
-	Redirects.Emplace(
-		ECoreRedirectFlags::Type_Class, TEXT("AGX_ShapeMaterialAsset"), TEXT("AGX_ShapeMaterial"));
-	Redirects.Emplace(
-		ECoreRedirectFlags::Type_Class, TEXT("AGX_ShapeMaterialInstance"),
-		TEXT("AGX_ShapeMaterial"));
-
-	// Terrain materials.
-	Redirects.Emplace(
-		ECoreRedirectFlags::Type_Class, TEXT("AGX_TerrainMaterialBase"),
-		TEXT("AGX_TerrainMaterial"));
-	Redirects.Emplace(
-		ECoreRedirectFlags::Type_Class, TEXT("AGX_TerrainMaterialAsset"),
-		TEXT("AGX_TerrainMaterial"));
-	Redirects.Emplace(
-		ECoreRedirectFlags::Type_Class, TEXT("AGX_TerrainMaterialInstance"),
-		TEXT("AGX_TerrainMaterial"));
+		ECoreRedirectFlags::Type_Property, TEXT("AGX_ExampleComponent.OldExampleProperty"),
+		TEXT("NewExampleProperty"));
 
 	FCoreRedirects::AddRedirectList(Redirects, TEXT("AGXUnreal"));
-	
+	*/
 }
 
 #undef LOCTEXT_NAMESPACE
