601f22f20455c4dd6fbb2ff94f26bbd3ceb9024a
Martin Nilsson <martin.nilsson@algoryx.se>
2022-10-21 17:24:29 +0200

    Remove mention of 9.8 from gravity force estimation in AGX_ContactMaterial.h

--- Source/AGXUnreal/Public/Materials/AGX_ContactMaterial.h
+++ Source/AGXUnreal/Public/Materials/AGX_ContactMaterial.h
@@ -162,7 +162,7 @@ public:
 	 * This should be set to an estimation of the force, in Newtons, by which the two colliding
 	 * objects are being pushed together. If the main contributor to this force is gravity then
 	 * this value should be set to the mass of the upper object and any additional load it is
-	 * carrying times the gravitational acceleration in m/s^2, i.e. around 9.8.
+	 * carrying times the gravitational acceleration in m/s^2.
 	 */
 	UPROPERTY(
 		EditAnywhere, Category = "Friction",
