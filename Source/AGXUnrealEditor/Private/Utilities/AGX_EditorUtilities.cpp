628e4c1911c3c0a9d27c09b4baf80d3b2d9c71bb
Josef Holmner <josef.holmner@algoryx.com>
2022-11-03 13:15:59 +0100

    Add SaveAsset function in EditorUtilities

--- Source/AGXUnrealEditor/Private/Utilities/AGX_EditorUtilities.cpp
+++ Source/AGXUnrealEditor/Private/Utilities/AGX_EditorUtilities.cpp
@@ -46,6 +46,44 @@
 
 #define LOCTEXT_NAMESPACE "FAGX_EditorUtilities"
 
+bool FAGX_EditorUtilities::SaveAsset(UObject& Asset)
+{
+	UPackage* Package = Asset.GetPackage();
+	if (Package == nullptr || Package->GetPathName().IsEmpty())
+	{
+		UE_LOG(
+			LogAGX, Error,
+			TEXT("SaveAsset called with Asset '%s' without a valid Package. The asset will not be "
+				 "saved."),
+			*Asset.GetName());
+		return false;
+	}
+
+	Asset.MarkPackageDirty();
+	Asset.PostEditChange();
+
+	const FString PackageFilename = FPackageName::LongPackageNameToFilename(
+		Asset.GetPackage()->GetPathName(), FPackageName::GetAssetPackageExtension());
+
+	// A package must have meta-data in order to be saved. It seems to be created automatically
+	// most of the time but sometimes, during unit tests for example, the engine tries to create it
+	// on-demand while saving the package which leads to a fatal error because this type of object
+	// look-up isn't allowed while saving packages. So try to force it here before calling
+	// SavePackage.
+	//
+	// The error message sometimes printed while within UPackage::SavePackage called below is:
+	// Illegal call to StaticFindObjectFast() while serializing object data or garbage collecting!
+	Package->GetMetaData();
+
+#if UE_VERSION_OLDER_THAN(5, 0, 0)
+	return UPackage::SavePackage(Package, &Asset, RF_NoFlags, *PackageFilename);
+#else
+	FSavePackageArgs SaveArgs;
+	SaveArgs.TopLevelFlags = RF_Public | RF_Standalone;
+	return UPackage::SavePackage(Package, &Asset, *PackageFilename, SaveArgs);
+#endif
+}
+
 std::tuple<AActor*, USceneComponent*> FAGX_EditorUtilities::CreateEmptyActor(
 	const FTransform& Transform, UWorld* World)
 {
