7177710bf9ef81ad996ec11343f682f44352aa78
Josef Holmner <josef.holmner@algoryx.com>
2023-03-08 07:02:30 +0100

    Update AGX_MergeSplitPropertiesBase.cpp

--- Source/AGXUnreal/Private/AMOR/AGX_MergeSplitPropertiesBase.cpp
+++ Source/AGXUnreal/Private/AMOR/AGX_MergeSplitPropertiesBase.cpp
@@ -18,7 +18,7 @@ FAGX_MergeSplitPropertiesBase& FAGX_MergeSplitPropertiesBase::operator=(
 
 bool FAGX_MergeSplitPropertiesBase::operator==(const FAGX_MergeSplitPropertiesBase& Other) const
 {
-	return GetNative() == Other.GetNative() && Other.bEnableMerge == bEnableMerge &&
+	return HasNative() == Other.HasNative() && Other.bEnableMerge == bEnableMerge &&
 		   Other.bEnableSplit == bEnableSplit;
 }
 
