79e4070822745e0e492cc67c04aea44f6e113e01
Martin Nilsson <martin.nilsson@algoryx.com>
2023-03-10 15:15:20 +0100

    Remove duplicated comment

--- Source/AGXUnrealEditor/Private/AGX_ImporterToBlueprint.cpp
+++ Source/AGXUnrealEditor/Private/AGX_ImporterToBlueprint.cpp
@@ -2267,13 +2267,6 @@ namespace AGX_ImporterToBlueprint_SynchronizeModel_helpers
 		const SCSNodeCollection& SCSNodes, FAGX_SimObjectsImporterHelper& Helper,
 		TArray<UObject*>& AssetsToDelete)
 	{
-		// Delete all render and collision meshes.
-		//
-		// We currently can't reuse the old assets because we have no way of knowing if they have
-		// been changed or not since we, unfortunately, store these meshes as Unreal Engine Static
-		// Mesh assets which Unreal Engine changes during import. For the render mesh we may not
-		// have a choice since they are for rendering, but the collision mesh should perhaps be
-		// a custom asset whose storage is byte-compatible with AGX Dynamics trimesh.
 		auto DeleteMeshAssets =
 			[&Helper, &AssetsToDelete](
 				const TMap<FGuid, USCS_Node*>& MeshComponents, const FString& DirectoryName)
