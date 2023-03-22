07b8b878b249012ca700da5f42cbfa1d61a2c996
Josef Holmner <josef.holmner@algoryx.com>
2023-03-22 13:51:05 +0100

    Cleanup code in TerrainDataSource

--- Source/AGXUnrealBarrier/Private/Terrain/TerrainDataSource.cpp
+++ Source/AGXUnrealBarrier/Private/Terrain/TerrainDataSource.cpp
@@ -8,13 +8,15 @@ using namespace agxTerrain;
 TerrainDataSource::TerrainHeightType FTerrainDataSource::fetchTerrainTile(
 	const TileSpecification& ts, TileId id)
 {
+	TerrainHeightType HeightsAGX;
+
 	if (HeightFetcher == nullptr)
 	{
 		UE_LOG(
 			LogAGX, Error,
 			TEXT("fetchTerrainTile called on a TerrainDataSource that does not have a "
 				 "HeightFetcher set. Fetchin Terrain Tile will fail."));
-		return TerrainHeightType();
+		return HeightsAGX;
 	}
 
 	const agx::Real TileSizeHalfAGX = ts.getTileSize() / 2.0;
@@ -23,15 +25,12 @@ TerrainDataSource::TerrainHeightType FTerrainDataSource::fetchTerrainTile(
 	const int32 VertCountSide = static_cast<int32>(ts.getTileResolution());
 
 	TArray<float> Heights;
-	const bool Result = HeightFetcher->FetchHeights(
-		TileCornerWorldPosUnreal, VertCountSide, VertCountSide, Heights);
-
-	if (!Result)
+	if (!HeightFetcher->FetchHeights(
+			TileCornerWorldPosUnreal, VertCountSide, VertCountSide, Heights))
 	{
-		return TerrainHeightType();
+		return HeightsAGX;
 	}
 
-	TerrainHeightType HeightsAGX;
 	HeightsAGX.reserve(Heights.Num());
 	for (float Height : Heights)
 	{
