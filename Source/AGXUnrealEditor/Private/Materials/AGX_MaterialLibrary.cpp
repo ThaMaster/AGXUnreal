e4095e59a9e9ce94531d17967f11e57c5dcae150
Josef <josef.holmner@algoryx.com>
2023-09-26 07:43:26 +0200

    Add back the comment about need to fully load package

--- Source/AGXUnrealEditor/Private/Materials/AGX_MaterialLibrary.cpp
+++ Source/AGXUnrealEditor/Private/Materials/AGX_MaterialLibrary.cpp
@@ -106,6 +106,18 @@ namespace AGX_MaterialLibrary_helpers
 		}
 
 		Asset->CopyFrom(Material);
+
+		// Must fully load the package or else project packaging will fail with:
+		//
+		//    Package /AGXUnreal/Terrain/TerrainMaterialLibrary/AGX_TM_gravel_1 supposed
+		//    to be fully loaded but isn't. RF_WasLoaded is set
+		//
+		//    Unable to cook package for platform because it is unable to be loaded:
+		//    <PATH>/AGXUnreal/Content/Terrain/TerrainMaterialLibrary/AGX_TM_gravel_1.uasset
+		//
+		// I'm not entirely sure where the FullyLoad call should be for it to
+		// take effect in all cases, so there are a few of them. Remove the
+		// unnecessary ones once we know which can safely be removed.
 		FAGX_ObjectUtilities::SaveAsset(*Asset, true);
 		return Asset;
 	}
