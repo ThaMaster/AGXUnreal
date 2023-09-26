a8cde71f76be3c9f3ec00d5faf6874ace46e7ead
Josef <josef.holmner@algoryx.com>
2023-09-26 07:35:28 +0200

    Run clang-format on MaterialLibraryBarrier.h

--- Source/AGXUnrealBarrier/Public/Materials/MaterialLibraryBarrier.h
+++ Source/AGXUnrealBarrier/Public/Materials/MaterialLibraryBarrier.h
@@ -13,14 +13,14 @@
 namespace AGX_MaterialLibraryBarrier
 {
 	AGXUNREALBARRIER_API TArray<FString> GetAvailableLibraryContactMaterials();
-	AGXUNREALBARRIER_API TOptional<FContactMaterialBarrier>
-	LoadContactMaterialProfile(const FString& MaterialName);
+	AGXUNREALBARRIER_API TOptional<FContactMaterialBarrier> LoadContactMaterialProfile(
+		const FString& MaterialName);
 
 	AGXUNREALBARRIER_API TArray<FString> GetAvailableLibraryShapeMaterials();
-	AGXUNREALBARRIER_API TOptional<FShapeMaterialBarrier>
-	LoadShapeMaterialProfile(const FString& MaterialName);
+	AGXUNREALBARRIER_API TOptional<FShapeMaterialBarrier> LoadShapeMaterialProfile(
+		const FString& MaterialName);
 
 	AGXUNREALBARRIER_API TArray<FString> GetAvailableLibraryTerrainMaterials();
-	AGXUNREALBARRIER_API TOptional<FTerrainMaterialBarrier>
-	LoadTerrainMaterialProfile(const FString& MaterialName);
+	AGXUNREALBARRIER_API TOptional<FTerrainMaterialBarrier> LoadTerrainMaterialProfile(
+		const FString& MaterialName);
 }
