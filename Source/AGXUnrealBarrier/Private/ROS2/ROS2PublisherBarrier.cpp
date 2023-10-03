44b03933f1bee4cb9433b46663d458b1d1741a45
Josef <josef.holmner@algoryx.com>
2023-10-03 15:40:59 +0200

    Add PublisherBarrier class

---
+++ Source/AGXUnrealBarrier/Private/ROS2/ROS2PublisherBarrier.cpp
@@ -0,0 +1,127 @@
+// Copyright 2023, Algoryx Simulation AB.
+
+#include "ROS2/ROS2PublisherBarrier.h"
+
+// AGX Dynamics for Unreal includes.
+#include "AGX_LogCategory.h"
+#include "AGXROS2Types.h"
+#include "ROS2/AGX_ROS2Messages.h"
+#include "ROS2/ROS2Conversions.h"
+
+
+// Helper macro to minimize amount of code needed in large switch-statement.
+// clang-format off
+#define AGX_SEND_ROS2_MSGS(PubType, MsgType)                                                       \
+{                                                                                              \
+	if (auto Pub = dynamic_cast<const PubType*>(Native.get()))                                 \
+	{                                                                                          \
+		Pub->Native->sendMessage(Convert(*static_cast<const MsgType*>(&Msg)));                 \
+	}                                                                                          \
+	else                                                                                       \
+	{                                                                                          \
+		UE_LOG(                                                                                \
+			LogAGX, Error,                                                                     \
+			TEXT(                                                                              \
+				"Unexpected internal error: unable to downcast to the correct Publisher type " \
+				"in FROS2PublisherBarrier::SendMessage. The message will not be sent."));      \
+	}                                                                                          \
+	return;                                                                                    \
+}
+// clang-format on
+
+
+FROS2PublisherBarrier::FROS2PublisherBarrier()
+{
+}
+
+FROS2PublisherBarrier::~FROS2PublisherBarrier()
+{
+	// Must provide a destructor implementation in the .cpp file because the
+	// std::unique_ptr Native's destructor must be able to see the definition,
+	// not just the forward declaration, of FROS2Publisher.
+}
+
+FROS2PublisherBarrier::FROS2PublisherBarrier(FROS2PublisherBarrier&& Other)
+{
+	*this = std::move(Other);
+}
+
+FROS2PublisherBarrier& FROS2PublisherBarrier::operator=(FROS2PublisherBarrier&& Other)
+{
+	Native = std::move(Other.Native);
+	Other.Native = nullptr;
+	MessageType = Other.MessageType;
+	return *this;
+}
+
+bool FROS2PublisherBarrier::HasNative() const
+{
+	return Native != nullptr;
+}
+
+void FROS2PublisherBarrier::AllocateNative(EAGX_ROS2MessageType InMessageType, const FString& Topic)
+{
+	using namespace StdMsgs;
+	using namespace agxIO::ROS2::stdMsgs;
+
+	MessageType = InMessageType;
+	switch (InMessageType)
+	{
+		case EAGX_ROS2MessageType::Invalid:
+			break; // Log error after this switch statement.
+		case EAGX_ROS2MessageType::StdMsgsFloat32:
+			Native = std::make_unique<FPublisherFloat32>(new PublisherFloat32(Convert(Topic)));
+			return;
+		case EAGX_ROS2MessageType::StdMsgsInt32:
+			Native = std::make_unique<FPublisherInt32>(new PublisherInt32(Convert(Topic)));
+			return;
+	}
+
+	UE_LOG(
+		LogAGX, Error,
+		TEXT("Unsupported or invalid type passed to FROS2PublisherBarrier::AllocateNative, topic: "
+			 "'%s'. "
+			 "Native will not be allocated."),
+		*Topic);
+}
+
+FROS2Publisher* FROS2PublisherBarrier::GetNative()
+{
+	return Native.get();
+}
+
+const FROS2Publisher* FROS2PublisherBarrier::GetNative() const
+{
+	return Native.get();
+}
+
+EAGX_ROS2MessageType FROS2PublisherBarrier::GetMessageType() const
+{
+	return MessageType;
+}
+
+void FROS2PublisherBarrier::SendMessage(const FAGX_ROS2Message& Msg) const
+{
+	check(HasNative());
+
+	switch (MessageType)
+	{
+		case EAGX_ROS2MessageType::Invalid:
+			break; // Log error after this switch statement.
+		case EAGX_ROS2MessageType::StdMsgsFloat32:
+			AGX_SEND_ROS2_MSGS(StdMsgs::FPublisherFloat32, FAGX_StdMsgsFloat32)
+		case EAGX_ROS2MessageType::StdMsgsInt32:
+			AGX_SEND_ROS2_MSGS(StdMsgs::FPublisherInt32, FAGX_StdMsgsInt32)
+	}
+
+	UE_LOG(
+		LogAGX, Error,
+		TEXT("FROS2PublisherBarrier::SendMessage called on PublisherBarrier with an invalid "
+			 "MessageType. The message will not be sent."));
+}
+
+void FROS2PublisherBarrier::ReleaseNative()
+{
+	Native = nullptr;
+	MessageType = EAGX_ROS2MessageType::Invalid;
+}
---
+++ Source/AGXUnrealBarrier/Public/ROS2/ROS2PublisherBarrier.h
@@ -0,0 +1,41 @@
+// Copyright 2023, Algoryx Simulation AB.
+
+#pragma once
+
+// AGX Dynamics for Unreal includes.
+#include "ROS2/AGX_ROS2Enums.h"
+
+// Standard library includes.
+#include <memory>
+
+struct FAGX_ROS2Message;
+struct FROS2Publisher;
+
+class AGXUNREALBARRIER_API FROS2PublisherBarrier
+{
+public:
+	FROS2PublisherBarrier();
+	~FROS2PublisherBarrier();
+	FROS2PublisherBarrier(FROS2PublisherBarrier&& Other);
+	FROS2PublisherBarrier& operator=(FROS2PublisherBarrier&& Other);
+
+	bool HasNative() const;
+
+	void AllocateNative(EAGX_ROS2MessageType InMessageType, const FString& Topic);
+
+	FROS2Publisher* GetNative();
+	const FROS2Publisher* GetNative() const;
+
+	void ReleaseNative();
+
+	EAGX_ROS2MessageType GetMessageType() const;
+	void SendMessage(const FAGX_ROS2Message& Msg) const;
+
+private:
+	FROS2PublisherBarrier(const FROS2PublisherBarrier&) = delete;
+	void operator=(const FROS2PublisherBarrier&) = delete;
+
+private:
+	std::unique_ptr<FROS2Publisher> Native;
+	EAGX_ROS2MessageType MessageType {EAGX_ROS2MessageType::Invalid};
+};
