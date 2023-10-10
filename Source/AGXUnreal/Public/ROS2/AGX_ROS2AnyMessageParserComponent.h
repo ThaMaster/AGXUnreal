05f879579d376fa106590a62a983567df5bb3fc8
Josef <josef.holmner@algoryx.com>
2023-10-10 12:40:39 +0200

    Implement read functions in AnyMessageParserComponent

--- Source/AGXUnreal/Public/ROS2/AGX_ROS2AnyMessageParserComponent.h
+++ Source/AGXUnreal/Public/ROS2/AGX_ROS2AnyMessageParserComponent.h
@@ -45,9 +45,87 @@ public:
 	UFUNCTION(BlueprintCallable, Category = "AGX ROS2 Any Message")
 	UAGX_ROS2AnyMessageParserComponent* BeginParse(const FAGX_AgxMsgsAny& Message);
 
-	/** The result is cast to an int32 internally. */
+	/** The result is cast to int32 internally. */
 	UFUNCTION(BlueprintCallable, Category = "AGX ROS2 Any Message")
 	int32 ReadInt8();
+
+	UFUNCTION(BlueprintCallable, Category = "AGX ROS2 Any Message")
+	uint8 ReadUInt8();
+
+	/** The result is cast to int32 internally. */
+	UFUNCTION(BlueprintCallable, Category = "AGX ROS2 Any Message")
+	int32 ReadInt16();
+
+	/** The result is cast to int32 internally. */
+	UFUNCTION(BlueprintCallable, Category = "AGX ROS2 Any Message")
+	int32 ReadUInt16();
+
+	UFUNCTION(BlueprintCallable, Category = "AGX ROS2 Any Message")
+	int32 ReadInt32();
+
+	/** The result is cast to int64 internally. */
+	UFUNCTION(BlueprintCallable, Category = "AGX ROS2 Any Message")
+	int64 ReadUInt32();
+
+	UFUNCTION(BlueprintCallable, Category = "AGX ROS2 Any Message")
+	int64 ReadInt64();
+
+	/** The result is cast to int64 internally. */
+	UFUNCTION(BlueprintCallable, Category = "AGX ROS2 Any Message")
+	int64 ReadUInt64();
+
+	UFUNCTION(BlueprintCallable, Category = "AGX ROS2 Any Message")
+	float ReadFloat32();
+
+	UFUNCTION(BlueprintCallable, Category = "AGX ROS2 Any Message")
+	double ReadDouble64();
+
+	UFUNCTION(BlueprintCallable, Category = "AGX ROS2 Any Message")
+	FString ReadString();
+
+	UFUNCTION(BlueprintCallable, Category = "AGX ROS2 Any Message")
+	bool ReadBool();
+
+	/** The result is cast to int32 internally. */
+	UFUNCTION(BlueprintCallable, Category = "AGX ROS2 Any Message")
+	TArray<int32> ReadInt8Sequence();
+
+	UFUNCTION(BlueprintCallable, Category = "AGX ROS2 Any Message")
+	TArray<uint8> ReadUInt8Sequence();
+
+	/** The result is cast to int32 internally. */
+	UFUNCTION(BlueprintCallable, Category = "AGX ROS2 Any Message")
+	TArray<int32> ReadInt16Sequence();
+
+	/** The result is cast to int32 internally. */
+	UFUNCTION(BlueprintCallable, Category = "AGX ROS2 Any Message")
+	TArray<int32> ReadUInt16Sequence();
+
+	UFUNCTION(BlueprintCallable, Category = "AGX ROS2 Any Message")
+	TArray<int32> ReadInt32Sequence();
+
+	/** The result is cast to int64 internally. */
+	UFUNCTION(BlueprintCallable, Category = "AGX ROS2 Any Message")
+	TArray<int64> ReadUInt32Sequence();
+
+	UFUNCTION(BlueprintCallable, Category = "AGX ROS2 Any Message")
+	TArray<int64> ReadInt64Sequence();
+
+	/** The result is cast to int64 internally. */
+	UFUNCTION(BlueprintCallable, Category = "AGX ROS2 Any Message")
+	TArray<int64> ReadUInt64Sequence();
+
+	UFUNCTION(BlueprintCallable, Category = "AGX ROS2 Any Message")
+	TArray<float> ReadFloat32Sequence();
+
+	UFUNCTION(BlueprintCallable, Category = "AGX ROS2 Any Message")
+	TArray<double> ReadDouble64Sequence();
+
+	UFUNCTION(BlueprintCallable, Category = "AGX ROS2 Any Message")
+	TArray<FString> ReadStringSequence();
+
+	UFUNCTION(BlueprintCallable, Category = "AGX ROS2 Any Message")
+	TArray<bool> ReadBoolSequence();
 
 private:
 	FROS2AnyMessageParserBarrier NativeBarrier;
