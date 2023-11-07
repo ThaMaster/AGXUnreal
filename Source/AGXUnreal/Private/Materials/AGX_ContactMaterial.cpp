3d81796116d026d84d743e9932da00a6be515afc
Josef <josef.holmner@algoryx.com>
2023-11-07 16:09:25 +0100

    Use registrar to get world in contactmaterial

--- Source/AGXUnreal/Private/Materials/AGX_ContactMaterial.cpp
+++ Source/AGXUnreal/Private/Materials/AGX_ContactMaterial.cpp
@@ -1254,17 +1254,16 @@ void UAGX_ContactMaterial::CreateNative(const UAGX_ContactMaterialRegistrarCompo
 	if (IsInstance())
 	{
 		check(!HasNative());
-		check(Registrar.GetWorld() == GetWorld());
-		UWorld* World = GetWorld();
+		UWorld* World = Registrar.GetWorld();
 		check(World != nullptr && World->IsGameWorld());
 
 		Material1 = Material1 != nullptr ? Material1->GetOrCreateInstance(World) : nullptr;
 		Material2 = Material2 != nullptr ? Material2->GetOrCreateInstance(World) : nullptr;
 
 		FShapeMaterialBarrier* MaterialBarrier1 =
-			Material1 != nullptr ? Material1->GetOrCreateShapeMaterialNative(GetWorld()) : nullptr;
+			Material1 != nullptr ? Material1->GetOrCreateShapeMaterialNative(World) : nullptr;
 		FShapeMaterialBarrier* MaterialBarrier2 =
-			Material2 != nullptr ? Material2->GetOrCreateShapeMaterialNative(GetWorld()) : nullptr;
+			Material2 != nullptr ? Material2->GetOrCreateShapeMaterialNative(World) : nullptr;
 
 		NativeBarrier.AllocateNative(MaterialBarrier1, MaterialBarrier2);
 		if (!HasNative())
