4057f094398764d3b68539155b7b647a6267a185
Martin Nilsson <martin.nilsson@algoryx.se>
2024-02-26 17:06:50 +0100

    Add formatted version of AGX_CHECK,AGX_CHECKF

--- Source/AGXUnrealBarrier/Public/AGX_Check.h
+++ Source/AGXUnrealBarrier/Public/AGX_Check.h
@@ -6,7 +6,9 @@
 #include "Misc/AssertionMacros.h"
 
 #if defined(AGXUNREAL_CHECK) && AGXUNREAL_CHECK == 1
-#define AGX_CHECK(x) check(x)
+#define AGX_CHECK(expr) check(expr)
+#define AGX_CHECKF(expr, format,  ...) checkf(expr, format, ##__VA_ARGS__)
 #else
 #define AGX_CHECK(x)
+#define AGX_CHECKF(exprt, format, ...)
 #endif
