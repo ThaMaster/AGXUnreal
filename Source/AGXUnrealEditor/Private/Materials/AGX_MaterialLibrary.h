c91f8e593dc93c33348f9fdad2b222fad4bea5e9
Josef <josef.holmner@algoryx.com>
2024-03-12 13:17:28 +0100

    Introduce ForceOverwrite which is only true for refresh button in gui

--- Source/AGXUnrealEditor/Private/Materials/AGX_MaterialLibrary.h
+++ Source/AGXUnrealEditor/Private/Materials/AGX_MaterialLibrary.h
@@ -4,14 +4,14 @@
 
 namespace AGX_MaterialLibrary
 {
-	bool InitializeShapeMaterialAssetLibrary();
+	bool InitializeShapeMaterialAssetLibrary(bool ForceOverwrite = false);
 
 	/**
 	 * Note : Shape Materials must be initialized before calling this function.
 	 * I.e. call InitializeShapeMaterialAssetLibrary before calling this function.
 	 * This is because the Contact Materials reference Shape Materials.
 	 */
-	bool InitializeContactMaterialAssetLibrary();
+	bool InitializeContactMaterialAssetLibrary(bool ForceOverwrite = false);
 
-	bool InitializeTerrainMaterialAssetLibrary();
+	bool InitializeTerrainMaterialAssetLibrary(bool ForceOverwrite = false);
 }
