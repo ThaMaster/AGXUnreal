2bb7feece4136506b35b8793d6e1404e1cdfe2d7
Josef <josef.holmner@algoryx.com>
2024-03-20 08:01:08 +0100

    Re-order functions to keep CoM offset stuff together

--- Source/AGXUnreal/Public/AGX_RigidBodyComponent.h
+++ Source/AGXUnreal/Public/AGX_RigidBodyComponent.h
@@ -96,14 +96,6 @@ public:
 	FVector GetCenterOfMassOffset() const;
 
 	/**
-	 * Returns the position of the center of mass in world coordinates [cm].
-	 * This function is only valid if the Rigid Body has a Native object, which usually is true only
-	 * during Play. Returns zero vector if not.
-	 */
-	UFUNCTION(BlueprintCallable, Category = "AGX Dynamics")
-	FVector GetCenterOfMassPosition() const;
-
-	/**
 	 * Whether the center of mass offset should be computed automatically.
 	 */
 	UPROPERTY(EditAnywhere, Category = "AGX Dynamics")
@@ -114,6 +106,14 @@ public:
 
 	UFUNCTION(BlueprintCallable, Category = "AGX Dynamics")
 	bool GetAutoGenerateCenterOfMassOffset() const;
+
+	/**
+	 * Returns the position of the center of mass in world coordinates [cm].
+	 * This function is only valid if the Rigid Body has a Native object, which usually is true only
+	 * during Play. Returns zero vector if not.
+	 */
+	UFUNCTION(BlueprintCallable, Category = "AGX Dynamics")
+	FVector GetCenterOfMassPosition() const;
 
 	UFUNCTION(BlueprintCallable, Category = "AGX Dynamics")
 	TArray<UAGX_ShapeComponent*> GetShapes() const;
