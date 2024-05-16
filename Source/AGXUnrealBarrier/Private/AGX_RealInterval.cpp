6ddd50c59f6ade70ee1850fa001b87794618e1af
Martin Nilsson <martin.nilsson@algoryx.com>
2024-05-16 09:40:00 +0200

    In FAGX_RealInterval::SerializeFromMismatchedTag, use GetParameterName instead of StructName
    Since StructName is deprecated.

--- Source/AGXUnrealBarrier/Private/AGX_RealInterval.cpp
+++ Source/AGXUnrealBarrier/Private/AGX_RealInterval.cpp
@@ -6,7 +6,9 @@
 #include "Utilities/DoubleInterval.h"
 
 // Unreal Engine includes.
+#include "Logging/StructuredLog.h"
 #include "Math/Interval.h"
+#include "Misc/EngineVersionComparison.h"
 
 bool FAGX_RealInterval::SerializeFromMismatchedTag(
 	struct FPropertyTag const& Tag, FStructuredArchive::FSlot Slot)
@@ -16,8 +18,13 @@ bool FAGX_RealInterval::SerializeFromMismatchedTag(
 		return false;
 	}
 
+#if UE_VERSION_OLDER_THAN(5, 4, 0)
+	const FName StructName = Tag.StructName;
+#else
+	const FName StructName = Tag.GetType().GetParameterName(0);
+#endif
 	UScriptStruct* FloatStruct = TBaseStructure<FFloatInterval>::Get();
-	if (Tag.StructName == FloatStruct->GetFName())
+	if (StructName == FloatStruct->GetFName())
 	{
 		// The archive has a Float Interval, conversion needed.
 		FFloatInterval Restored;
@@ -28,7 +35,7 @@ bool FAGX_RealInterval::SerializeFromMismatchedTag(
 	}
 
 	UScriptStruct* DoubleStruct = FAGX_DoubleInterval::StaticStruct();
-	if (Tag.StructName == DoubleStruct->GetFName())
+	if (StructName == DoubleStruct->GetFName())
 	{
 		/// @todo Make sure this code works. What we can test on? Where is FAGX_DoubleInterval used
 		/// as a Property?
