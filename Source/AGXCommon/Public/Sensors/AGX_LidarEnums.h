46e95aaefce5628944661397cd8657d5ae60b3a9
Josef <josef.holmner@algoryx.com>
2024-09-12 08:04:06 +0200

    Non enum class
    [ci skip]

--- Source/AGXCommon/Public/Sensors/AGX_LidarEnums.h
+++ Source/AGXCommon/Public/Sensors/AGX_LidarEnums.h
@@ -7,28 +7,28 @@
 
 /** Specifies the model of the Lidar. */
 UENUM(BlueprintType)
-enum class EAGX_LidarModel
+enum EAGX_LidarModel
 {
 	/** Lidar uses a custom ray pattern where the user provides the ray pattern rays. */
 	CustomRayPattern = 0,
 
 	/** Generic Lidar with a Horizontal Sweep ray pattern. */
-	GenericHorizontalSweep = 10,
+	GenericHorizontalSweep = 5,
 
 	/** Ouster OS0 */
-	OusterOS0 = 20,
+	OusterOS0 = 10,
 
 	/** Ouster OS1 */
-	OusterOS1 = 30,
+	OusterOS1 = 15,
 
 	/** Ouster OS2 */
-	OusterOS2 = 40,
+	OusterOS2 = 20,
 };
 
 /** Specifies which Components the Lidar Surface Material Component applies the Lidar Surface
  * Material to. */
 UENUM(BlueprintType)
-enum class EAGX_LidarSurfaceMaterialAssignmentSelection
+enum EAGX_LidarSurfaceMaterialAssignmentSelection
 {
 	/**
 	 * Assign the Lidar Surface Material to the parent Component. Searches up the Component hierarcy
@@ -50,7 +50,7 @@ enum class EAGX_LidarSurfaceMaterialAssignmentSelection
 
 /** Number of channels setting for OusterOS Lidars. */
 UENUM(BlueprintType)
-enum class EAGX_OusterOSChannelCount
+enum EAGX_OusterOSChannelCount
 {
 	Ch_32,
 	Ch_64,
@@ -59,7 +59,7 @@ enum class EAGX_OusterOSChannelCount
 
 /** Channel distribution setting for OusterOS Lidars. */
 UENUM(BlueprintType)
-enum class EAGX_OusterOSChannelDistribution
+enum EAGX_OusterOSChannelDistribution
 {
 	Uniform,
 	AboveHorizon,
@@ -68,7 +68,7 @@ enum class EAGX_OusterOSChannelDistribution
 
 /** Horizontal resolution setting for OusterOS Lidars. */
 UENUM(BlueprintType)
-enum class EAGX_OusterOSHorizontalResolution
+enum EAGX_OusterOSHorizontalResolution
 {
 	Hr_512,
 	Hr_1024,
@@ -77,7 +77,7 @@ enum class EAGX_OusterOSHorizontalResolution
 
 /** Frequency setting for OusterOS Lidars. */
 UENUM(BlueprintType)
-enum class EAGX_OusterOSFrequency
+enum EAGX_OusterOSFrequency
 {
 	F_10,
 	F_20
