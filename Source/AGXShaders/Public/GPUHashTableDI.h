d6f3fd6f00f2fb8edc1b47808d18fea8ab181692
Christoffer Nordlander <christoffernordlander02@gmail.com>
2025-02-24 16:01:54 +0100

    Add Custom Data Interface to Niagara.

--- Source/AGXShaders/Public/GPUHashTableDI.h
+++ Source/AGXShaders/Public/GPUHashTableDI.h
@@ -2,13 +2,10 @@
 
 #include "CoreMinimal.h"
 #include "NiagaraCommon.h"
-#include "NiagaraShared.h"
 #include "NiagaraTypes.h"
 #include "NiagaraDataInterface.h"
-#include "NiagaraDataInterfaceRW.h"
-#include "NiagaraDataInterfaceArrayFunctionLibrary.h"
-#include "AGX_Real.h"
 #include "GPUHashTableDI.generated.h"
+
 
 UCLASS(EditInlineNew, Category = "Niagara", CollapseCategories, meta = (DisplayName = "GPU Hash Table"))
 class AGXSHADERS_API UGPUHashTableDI : public UNiagaraDataInterface
@@ -16,9 +13,22 @@ class AGXSHADERS_API UGPUHashTableDI : public UNiagaraDataInterface
 	GENERATED_UCLASS_BODY()
 
 public:
+	// Registers the interface so it becoems available in editor
+	virtual void PostInitProperties() override;
 
-	void PostInitProperties();
+	// Creates the functions that are available in the editor
 	virtual void GetFunctions(TArray<FNiagaraFunctionSignature>& OutFunctions) override;
-	//virtual bool InitPerInstanceData(void* PerInstanceData, FNiagaraSystemInstance* SystemInstance) override;
-	//virtual void DestroyPerInstanceData(void* PerInstanceData, FNiagaraSystemInstance* SystemInstance) override;
+	virtual void GetVMExternalFunction(const FVMExternalFunctionBindingInfo& BindingInfo, void* InstanceData, FVMExternalFunction& OutFunc) override;
+
+	virtual void VMLookupRoom(FVectorVMExternalFunctionContext& Context);
+
+private:
+	static const FName InsertIndex;
+	static const FName DeleteIndex;
+	static const FName AddRoomAtIndex;
+	static const FName ClearTable;
+	static const FName LookupRoom;
+	static const FName LookupPosition;
+	static const FName LookupVelocity;
+	static const FName LookupBounds;
 };
