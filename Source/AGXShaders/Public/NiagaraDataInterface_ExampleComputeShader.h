7657de524cd4066189cc9c3287b55320f0dce0d5
Christoffer Nordlander <christoffernordlander02@gmail.com>
2025-03-06 13:52:43 +0100

    Get the 'GetMousePosition' example data interrface working.

---
+++ Source/AGXShaders/Public/NiagaraDataInterface_ExampleComputeShader.h
@@ -0,0 +1,74 @@
+#pragma once
+
+#include "CoreMinimal.h"
+#include "NiagaraCommon.h"
+#include "NiagaraTypes.h"
+#include "NiagaraDataInterface.h"
+#include "NiagaraDataInterfaceRW.h"
+#include "NiagaraDataInterface_ExampleComputeShader.generated.h"
+
+UCLASS(EditInlineNew, Category = "Compute Shader", CollapseCategories, meta = (DisplayName = "Example Compute Shader"))
+class AGXSHADERS_API UNiagaraDataInterface_ExampleComputeShader : public UNiagaraDataInterface
+{
+	GENERATED_UCLASS_BODY()
+
+	BEGIN_SHADER_PARAMETER_STRUCT(FShaderParameters, )
+		SHADER_PARAMETER(FVector4f, MousePosition)
+	END_SHADER_PARAMETER_STRUCT()
+public:
+
+	// Runs once to set up the data interface
+	virtual void PostInitProperties() override;
+
+	// UNiagaraDataInterface Interface
+	virtual void GetVMExternalFunction(
+		const FVMExternalFunctionBindingInfo& BindingInfo, void* InstanceData,
+		FVMExternalFunction& OutFunc) override;
+	virtual bool CanExecuteOnTarget(ENiagaraSimTarget Target) const override
+	{
+		return true;
+	}
+#if WITH_EDITORONLY_DATA
+	virtual bool AppendCompileHash(FNiagaraCompileHashVisitor* InVisitor) const override;
+	virtual bool GetFunctionHLSL(
+		const FNiagaraDataInterfaceGPUParamInfo& ParamInfo,
+		const FNiagaraDataInterfaceGeneratedFunction& FunctionInfo, int FunctionInstanceIndex,
+		FString& OutHLSL) override;
+	virtual void GetParameterDefinitionHLSL(
+		const FNiagaraDataInterfaceGPUParamInfo& ParamInfo, FString& OutHLSL) override;
+#endif
+	virtual void BuildShaderParameters(
+		FNiagaraShaderParametersBuilder& ShaderParametersBuilder) const override;
+	virtual void SetShaderParameters(
+		const FNiagaraDataInterfaceSetShaderParametersContext& Context) const override;
+
+	virtual bool InitPerInstanceData(
+		void* PerInstanceData, FNiagaraSystemInstance* SystemInstance) override;
+	virtual void DestroyPerInstanceData(
+		void* PerInstanceData, FNiagaraSystemInstance* SystemInstance) override;
+	virtual int32 PerInstanceDataSize() const override;
+	virtual bool HasPreSimulateTick() const override
+	{
+		return true;
+	}
+	virtual bool PerInstanceTick(
+		void* PerInstanceData, FNiagaraSystemInstance* SystemInstance, float DeltaSeconds) override;
+	virtual void ProvidePerInstanceDataForRenderThread(
+		void* DataForRenderThread, void* PerInstanceData,
+		const FNiagaraSystemInstanceID& SystemInstance) override;
+	// UNiagaraDataInterface Interface
+
+	void GetMousePositionVM(FVectorVMExternalFunctionContext& Context);
+
+protected:
+
+#if WITH_EDITORONLY_DATA
+	virtual void GetFunctionsInternal(
+		TArray<FNiagaraFunctionSignature>& OutFunctions) const override;
+	
+#endif
+private:
+	static const FName GetValueName;
+	static const FName StoreValueName;
+	static const FName GetMousePositionName;
+};
