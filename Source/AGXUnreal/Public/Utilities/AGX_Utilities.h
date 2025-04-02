0baa41a1a1015a1597499916bc9fc02c174c35cf
Josef Holmner <josef.holmner@algoryx.com>
2025-04-02 11:47:24 +0200

    Add clarificaiton about paths in AGX_Utilities

--- Source/AGXUnreal/Public/Utilities/AGX_Utilities.h
+++ Source/AGXUnreal/Public/Utilities/AGX_Utilities.h
@@ -65,7 +65,7 @@ public:
 	 * Import an .agx archive or Urdf file and get a AActor template back.
 	 * This template AActor must be instantiated to be part of the world, see InstantiateActor.
 	 * The FilePath of the ImportSettings struct may be absolute or relative to the current Projects
-	 * root directory.
+	 * root directory or stand-alone application root directory.
 	 */
 	UFUNCTION(
 		BlueprintCallable, Category = "AGX Import", meta = (WorldContext = "WorldContextObject"))
