d6f84062ac55649afe2709dbfb47f2bc0e6a7d77
Christoffer Nordlander <christoffernordlander02@gmail.com>
2025-06-12 11:19:25 +0200

    Add base class for the Terrain Particle Renderer Components

---
+++ Source/AGXUnreal/Private/Terrain/ParticleRendering/AGX_BaseTerrainParticleRendererComponent.cpp
@@ -0,0 +1,129 @@
+// Copyright 2025, Algoryx Simulation AB.
+
+#include "Terrain/ParticleRendering/AGX_BaseTerrainParticleRendererComponent.h"
+
+// AGX Dynamics for Unreal includes.
+#include "AGX_PropertyChangedDispatcher.h"
+#include "AGX_LogCategory.h"
+
+// Unreal Engine includes.
+
+
+UAGX_BaseTerrainParticleRendererComponent::UAGX_BaseTerrainParticleRendererComponent()
+{
+	// Constructor
+}
+
+void UAGX_BaseTerrainParticleRendererComponent::BeginPlay()
+{
+	Super::BeginPlay();
+
+	if (!bEnableParticleRendering)
+	{
+		return;
+	}
+
+	if (!ParentTerrainActor)
+	{
+		// Fetch the parent terrain actor which holds the particle data
+		if (!InitializeParentTerrainActor())
+		{
+			return;
+		}
+	}
+
+	// Maybe remove this to allow user to bind to whatever they want?
+	BindParticleHandler();
+}
+
+void UAGX_BaseTerrainParticleRendererComponent::EndPlay(const EEndPlayReason::Type EndPlayReason)
+{
+	Super::EndPlay(EndPlayReason);
+	UnbindParticleHandler();
+}
+
+void UAGX_BaseTerrainParticleRendererComponent::SetEnableParticleRendering(bool bEnabled)
+{
+	bEnableParticleRendering = bEnabled;
+}
+
+bool UAGX_BaseTerrainParticleRendererComponent::InitializeParentTerrainActor()
+{
+	// First get parent actor
+	AActor* Owner = GetOwner();
+	if (!Owner)
+	{
+		UE_LOG(
+			LogAGX, Warning,
+			TEXT("Particle Renderer '%s' could not fetch the parent actor"
+				 "particles"),
+			*GetName());
+		return false;
+	}
+
+	// Then cast it to the proper type
+	ParentTerrainActor = Cast<AAGX_Terrain>(Owner);
+	if (!ParentTerrainActor)
+	{
+		UE_LOG(
+			LogAGX, Warning,
+			TEXT("Particle Renderer '%s' could not cast parent to 'AGX_Terrain' actor, cannot "
+				 "fetch particle data"
+				 "particles"),
+			*GetName());
+		return false;
+	}
+
+	return ParentTerrainActor != nullptr;
+}
+
+// TODO: Make it possible to choose which delegate and function to bind to!
+void UAGX_BaseTerrainParticleRendererComponent::BindParticleHandler()
+{
+	DelegateHandle = ParentTerrainActor->UpdateParticleDataDelegate.AddLambda(
+		[this](FParticleDataById data) { HandleParticleData(data); }
+	);
+}
+
+void UAGX_BaseTerrainParticleRendererComponent::UnbindParticleHandler()
+{
+	if (DelegateHandle.IsValid())
+	{
+		ParentTerrainActor->UpdateParticleDataDelegate.Remove(DelegateHandle);
+	}
+}
+
+void UAGX_BaseTerrainParticleRendererComponent::HandleParticleData(FParticleDataById data)
+{
+	return;
+}
+
+#if WITH_EDITOR
+
+void UAGX_BaseTerrainParticleRendererComponent::PostEditChangeChainProperty(
+	FPropertyChangedChainEvent& Event)
+{
+	FAGX_PropertyChangedDispatcher<ThisClass>::Get().Trigger(Event);
+	Super::PostEditChangeChainProperty(Event);
+}
+
+void UAGX_BaseTerrainParticleRendererComponent::PostInitProperties()
+{
+	Super::PostInitProperties();
+	InitPropertyDispatcher();
+}
+
+void UAGX_BaseTerrainParticleRendererComponent::InitPropertyDispatcher()
+{
+	FAGX_PropertyChangedDispatcher<ThisClass>& PropertyDispatcher =
+		FAGX_PropertyChangedDispatcher<ThisClass>::Get();
+	if (PropertyDispatcher.IsInitialized())
+	{
+		return;
+	}
+	PropertyDispatcher.Add(
+		AGX_MEMBER_NAME(bEnableParticleRendering),
+		[](ThisClass* This) { This->SetEnableParticleRendering(This->bEnableParticleRendering); });
+}
+
+#endif
---
+++ Source/AGXUnreal/Public/Terrain/ParticleRendering/AGX_BaseTerrainParticleRendererComponent.h
@@ -0,0 +1,52 @@
+// Copyright 2025, Algoryx Simulation AB.
+
+#pragma once
+
+// AGX Dynamics for Unreal includes.
+#include "Terrain/TerrainParticleTypes.h"
+#include "Terrain/AGX_Terrain.h"
+
+// Unreal Engine includes.
+#include "Components/ActorComponent.h"
+#include "CoreMinimal.h"
+#include "CoreTypes.h"
+
+#include "AGX_BaseTerrainParticleRendererComponent.generated.h"
+
+UCLASS(meta=(ShortToolTip="This class is a reusable component for handling of rendering AGX_Terrain particles."))
+class AGXUNREAL_API UAGX_BaseTerrainParticleRendererComponent : public UActorComponent
+{
+	GENERATED_BODY()
+
+public:
+
+	UAGX_BaseTerrainParticleRendererComponent();
+
+	/** Whether soil particles should be rendered or not. */
+	UPROPERTY(EditAnywhere, Category = "AGX Particle Rendering")
+	bool bEnableParticleRendering = true;
+
+	UFUNCTION(BlueprintCallable, Category = "AGX Particle Rendering")
+	void SetEnableParticleRendering(bool bEnabled);
+
+	virtual void BeginPlay() override;
+	virtual void EndPlay(const EEndPlayReason::Type EndPlayReason) override;
+
+private:
+
+	AAGX_Terrain* ParentTerrainActor = nullptr;
+	FDelegateHandle DelegateHandle;
+	
+	bool InitializeParentTerrainActor();
+
+	virtual void HandleParticleData(FParticleDataById data);
+
+	void BindParticleHandler();
+	void UnbindParticleHandler();
+
+#if WITH_EDITOR
+	virtual void PostInitProperties() override;
+	virtual void PostEditChangeChainProperty(FPropertyChangedChainEvent& Event) override;
+	void InitPropertyDispatcher();
+#endif
+};
